aa35fe0def9069e48602a3b1a59669e9bac48063
Vitor Pamplona <vitor@vitorpamplona.com>
2020-03-07 19:06:12 -0500

    Building SQL Database for the Locations

--- app/models/Database.js
+++ app/models/Database.js
@@ -1,3 +1,117 @@
 import SQLite from "react-native-sqlite-storage";
 SQLite.DEBUG(true);
 SQLite.enablePromise(true);
+
+const database_name = "safepaths.db";
+const database_version = "1.0";
+const database_displayname = "SQLite SafePaths Database";
+const database_size = 200000;
+
+export default class Database {
+    initDB() {
+        let db;
+        return new Promise((resolve) => {
+          console.log("Database integrity check ...");
+          SQLite.echoTest()
+            .then(() => {
+              console.log("Integrity check passed ...");
+              console.log("Opening database ...");
+              SQLite.openDatabase(
+                database_name,
+                database_version,
+                database_displayname,
+                database_size
+              )
+                .then(DB => {
+                  db = DB;
+                  console.log("Database OPEN");
+                  db.executeSql('SELECT 1 FROM Location LIMIT 1').then(() => {
+                      console.log("Database is ready ... executing query ...");
+                  }).catch((error) =>{
+                      console.log("Received error: ", error);
+                      console.log("Database not yet ready ... populating data");
+                      db.transaction((tx) => {
+                          tx.executeSql('CREATE TABLE IF NOT EXISTS Location (id INTEGER PRIMARY KEY AUTOINCREMENT, time BIGINT, latitude DOUBLE, longitude DOUBLE)');
+                      }).then(() => {
+                          console.log("Table created successfully");
+                      }).catch(error => {
+                          console.log(error);
+                      });
+                  });
+                  resolve(db);
+                })
+                .catch(error => {
+                  console.log(error);
+                });
+            })
+            .catch(error => {
+              console.log("echoTest failed - plugin not functional");
+            });
+        });
+    };
+
+    closeDatabase(db) {
+        if (db) {
+            console.log("Closing DB");
+            db.close()
+            .then(status => {
+                console.log("Database CLOSED");
+            })
+            .catch(error => {
+               console.log(error);
+            });
+        } else {
+            console.log("Database was not OPENED");
+        }
+    };
+
+    listLocations() {
+        return new Promise((resolve) => {
+          const locations = [];
+          this.initDB().then((db) => {
+            db.transaction((tx) => {
+              tx.executeSql('SELECT p.id, p.time, p.latitude, p.longitude FROM Location p', []).then(([tx,results]) => {
+                console.log("Query completed");
+                var len = results.rows.length;
+                for (let i = 0; i < len; i++) {
+                  let row = results.rows.item(i);
+                  const { id, time, latitude, longitude } = row;
+                  locations.push({
+                    id,
+                    time,
+                    latitude,
+                    longitude
+                  });
+                }
+                console.log(locations);
+                resolve(locations);
+              });
+            }).then((result) => {
+              this.closeDatabase(db);
+            }).catch((err) => {
+              console.log(err);
+            });
+          }).catch((err) => {
+            console.log(err);
+          });
+        });  
+    }
+
+    addLocation(loc) {
+        return new Promise((resolve) => {
+          this.initDB().then((db) => {
+            db.transaction((tx) => {
+              tx.executeSql('INSERT INTO Location VALUES (?, ?, ?, ?)', [loc.id, loc.time, loc.latitude, loc.longitude]).then(([tx, results]) => {
+                resolve(results);
+              });
+            }).then((result) => {
+              this.closeDatabase(db);
+            }).catch((err) => {
+              console.log(err);
+            });
+          }).catch((err) => {
+            console.log(err);
+          });
+        });  
+    }
+}
