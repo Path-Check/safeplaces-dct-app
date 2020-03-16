36383a4508d412c4e69da9fd355606f3f77c00bd
Steve Penrod <github@gpll.org>
2020-03-16 03:11:09 -0500

    Add missing notifications for Location Services
    Several conditions cause Private Kit to stop failing when it cannot access
    GPS info.  We should notify the user when this has happened.  That includes:
    * The app doesn't have proper permissions
    * The permissions have been revoked
    * The location service has been shut down
    
    Still to do is a notification to show the user that the background service
    is not doing anything when the user turns off location services.

--- app/views/News.js
+++ app/views/News.js
@@ -7,8 +7,7 @@ import {
     ScrollView,
     Linking,
     View,
-    Text,
-    Alert
+    Text
 } from 'react-native';
 
 import colors from "../constants/colors";
@@ -28,17 +27,37 @@ class NewsScreen extends Component {
 
 
     render() {
-        return (
-            <SafeAreaView style={styles.container}>
-                <View style={styles.headerContainer}>
-                    <Text onPress={() => this.backToMain()} style={styles.backArrow}> &#8249; </Text>
-                    <Text style={styles.sectionDescription}>Latest News</Text>
-                </View>
-                <WebView
-                    source= {{ uri: 'https://privatekit.mit.edu/views' }}
-                    style= {{ marginTop: 15}}
-                />
-            </SafeAreaView>
+        return ( <
+            SafeAreaView style = {
+                styles.container
+            } >
+            <
+            View style = {
+                styles.headerContainer
+            } >
+            <
+            Text onPress = {
+                () => this.backToMain()
+            }
+            style = {
+                styles.backArrow
+            } > & #8249; </Text>
+                    <Text style= {
+                styles.sectionDescription
+            } > Latest News < /Text> <
+            /View> <
+            WebView source = {
+                {
+                    uri: 'https://privatekit.mit.edu/views'
+                }
+            }
+            style = {
+                {
+                    marginTop: 15
+                }
+            }
+            /> <
+            /SafeAreaView>
         )
     }
 }
