fad473dbf206c13a69df9313393451dd6456c61b
Harsh Vitra <harshvitra@gmail.com>
2020-03-17 17:45:25 +0530

    Moved Styles

--- app/views/welcomeScreens/Intro2.js
+++ app/views/welcomeScreens/Intro2.js
@@ -1,136 +1,52 @@
 import React from 'react'
-import { View, Text,Dimensions, Image, TouchableOpacity } from 'react-native'
+import { View, Text,Dimensions, TouchableOpacity,StyleSheet } from 'react-native'
 const width = Dimensions.get('window').width;
 
 const Intro2 = (props) => {
     return (
-        <View style={{flex:1,backgroundColor:'white'}}>
-            <View style={{width:width*0.7866,backgroundColor:'#3497fc',height:'70%',borderRadius:12,alignSelf:'center',marginTop:'9%',justifyContent:'center'}}>
-                <Text 
-                    style={{
-                        fontFamily: "OpenSans-Bold",
-                        fontSize: 24,
-                        lineHeight: 55,
-                        letterSpacing: 0,
-                        textAlign: "center",
-                        color: "#ffffff"
-                        }}>
+        <View style={styles.mainContainer}>
+            <View style={styles.infoCard}>
+                <Text  style={styles.infoCardHeadText}>
                 Less than 100KB
                 </Text>
                 <Text 
-                style={{
-                    opacity: 0.8,
-                    fontFamily: "OpenSans-SemiBold",
-                    fontSize: 14,
-                    lineHeight: 24,
-                    letterSpacing: 0,
-                    textAlign: "center",
-                    color: "#ffffff",
-                    maxWidth:'84%',
-                    alignSelf:'center',
-                    marginTop:12}}>
+                style={styles.infoCardBodyText}>
 Private Kit’s trail generator logs your device’s location data in under 100KB of space – less space than a single picture. 
                 </Text>
 
                 <Text 
-                    style={{
-                        fontFamily: "OpenSans-Bold",
-                        fontSize: 24,
-                        lineHeight: 55,
-                        letterSpacing: 0,
-                        textAlign: "center",
-                        color: "#ffffff",
-                        marginTop:36
-                        }}>
+                    style={styles.infoCardHeadText}>
                 You are in charge
                 </Text>
                 <Text 
-                style={{
-                    opacity: 0.8,
-                    fontFamily: "OpenSans-SemiBold",
-                    fontSize: 14,
-                    lineHeight: 24,
-                    letterSpacing: 0,
-                    textAlign: "center",
-                    color: "#ffffff",
-                    maxWidth:'84%',
-                    alignSelf:'center',
-                    marginTop:12}}>
+                style={styles.infoCardBodyText}>
 Data Never Leaves Your Device Without Your Consent                </Text>
             </View>
            
-           <View style={{flexDirection:'row',left:width*.445,marginTop:30}}>
+           <View style={styles.navigationDotsView}>
            <TouchableOpacity 
            onPress={()=>props.navigation.navigate('Intro1')}
-               style={{
-                width: 8,
-                height: 8,
-                borderRadius: 13,
-                backgroundColor: "#78849e",
-                opacity:.32,
-                marginRight:8
-            }}/>
+               style={styles.inactiveIndicator}/>
                <View 
-               style={{
-                width: 8,
-                height: 8,
-                opacity: 1,
-                borderRadius: 13,
-                backgroundColor: "#665EFF",
-                marginRight:8
-            }}/>
+               style={styles.activeIndicator}/>
             <TouchableOpacity 
             onPress={()=>props.navigation.navigate('Intro3')}
-               style={{
-                width: 8,
-                height: 8,
-                opacity: 0.32,
-                borderRadius: 13,
-                backgroundColor: "#78849e"
-            }}/>
+               style={styles.inactiveIndicator}/>
            </View>
 
-            <View style={{flexDirection:'row',justifyContent:'space-between',width:width*.7866,alignSelf:'center'}}>
+            <View style={styles.buttonContainer}>
             <TouchableOpacity 
             onPress={()=>props.navigation.navigate('Intro1')}
-            style={{
-                borderRadius: 12,
-                backgroundColor: "rgba(120, 132, 158, 0.16)",
-                height:52,
-                alignSelf:'center',
-                width:width*.38,
-                marginTop:30,
-                justifyContent:'center'
-                }} >
+            style={styles.secondaryButtonTouchable} >
                 <Text 
-                    style={{
-                        fontFamily: "OpenSans-Bold",
-                        fontSize: 14,
-                        lineHeight: 19,
-                        letterSpacing: 0,
-                        textAlign: "center",
-                        color: "#454f63"}}>BACK</Text>
+                    style={styles.secondaryButtonText}>BACK</Text>
             </TouchableOpacity>
           
            <TouchableOpacity 
            onPress={()=>props.navigation.navigate('Intro3')}
-            style={{
-                borderRadius: 12,
-                backgroundColor: "#665eff",
-                height:52,
-                alignSelf:'center',
-                width:width*.38,
-                marginTop:30,
-                justifyContent:'center'
-                }} >
+            style={styles.primaryButtonTouchable} >
                 <Text 
-                    style={{
-                        fontFamily: "OpenSans-Bold",
-                        fontSize: 14,
-                        lineHeight: 19,
-                        letterSpacing: 0,
-                        textAlign: "center",
-                        color: "#ffffff"}}>NEXT</Text>
+                    style={styles.primaryButtonText}>NEXT</Text>
             </TouchableOpacity>
             </View>
           
@@ -138,5 +54,86 @@ Data Never Leaves Your Device Without Your Consent                </Text>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    
+    mainContainer:{
+        flex:1,
+        backgroundColor:'white'
+    },
+    infoCard: {width:width*0.7866,backgroundColor:'#3497fc',height:'70%',borderRadius:12,alignSelf:'center',marginTop:'9%',justifyContent:'center'},
+    infoCardImage:{alignSelf:'center',width:width*.5,height:width*.5,marginTop:'16%'},
+    infoCardHeadText:{
+        fontFamily: "OpenSans-Bold",
+        fontSize: 24,
+        lineHeight: 55,
+        letterSpacing: 0,
+        textAlign: "center",
+        color: "#ffffff"
+        },
+        infoCardBodyText:{
+            opacity: 0.8,
+            fontFamily: "OpenSans-SemiBold",
+            fontSize: 14,
+            lineHeight: 24,
+            letterSpacing: 0,
+            textAlign: "center",
+            color: "#ffffff",
+            maxWidth:'84%',
+            alignSelf:'center',
+            marginTop:20},
+    navigationDotsView:{flexDirection:'row',left:width*.445,marginTop:30},
+    activeIndicator:{
+        width: 8,
+        height: 8,
+        borderRadius: 13,
+        backgroundColor: "#665EFF",
+        opacity:1,
+        marginRight:8
+    },
+    inactiveIndicator:{
+        width: 8,
+        height: 8,
+        opacity: 0.32,
+        borderRadius: 13,
+        backgroundColor: "#78849e",
+        marginRight:8
+    },
+    primaryButtonTouchable:{
+        borderRadius: 12,
+        backgroundColor: "#665eff",
+        height:52,
+        alignSelf:'center',
+        width:width*.38,
+        marginTop:30,
+        justifyContent:'center'
+        },
+        primaryButtonText:{
+            fontFamily: "OpenSans-Bold",
+            fontSize: 14,
+            lineHeight: 19,
+            letterSpacing: 0,
+            textAlign: "center",
+            color: "#ffffff"},
+    buttonContainer:{flexDirection:'row',justifyContent:'space-between',width:width*.7866,alignSelf:'center'},
+    secondaryButtonTouchable:{
+        borderRadius: 12,
+        backgroundColor: "rgba(120, 132, 158, 0.16)",
+        height:52,
+        alignSelf:'center',
+        width:width*.38,
+        marginTop:30,
+        justifyContent:'center'
+        },
+        secondaryButtonText:{
+            fontFamily: "OpenSans-Bold",
+            fontSize: 14,
+            lineHeight: 19,
+            letterSpacing: 0,
+            textAlign: "center",
+            color: "#454f63"}
+});
+
 
 export default Intro2
--- app/views/welcomeScreens/Intro3.js
+++ app/views/welcomeScreens/Intro3.js
@@ -1,42 +1,20 @@
 import React from 'react'
-import { View, Text,Dimensions, Image, TouchableOpacity } from 'react-native'
+import { View, Text,Dimensions, Image, TouchableOpacity,StyleSheet } from 'react-native'
 const width = Dimensions.get('window').width;
 
 const Intro2 = (props) => {
     return (
-        <View style={{flex:1,backgroundColor:'white'}}>
-            <View style={{width:width*0.7866,backgroundColor:'#3497fc',height:'70%',borderRadius:12,alignSelf:'center',marginTop:'9%',justifyContent:'center'}}>
+        <View style={styles.mainContainer}>
+            <View style={styles.infoCard}>
                 <Text 
-                    style={{
-                        fontFamily: "OpenSans-Bold",
-                        fontSize: 24,
-                        lineHeight: 55,
-                        letterSpacing: 0,
-                        textAlign: "center",
-                        color: "#ffffff"
-                        }}>
+                    style={styles.infoCardHeadText}>
                 The Future
                 </Text>
                 <Text 
-                style={{
-                    opacity: 0.8,
-                    fontFamily: "OpenSans-SemiBold",
-                    fontSize: 14,
-                    lineHeight: 24,
-                    letterSpacing: 0,
-                    textAlign: "center",
-                    color: "#ffffff",
-                    maxWidth:'84%',
-                    alignSelf:'center',
-                    marginTop:12}}>
+                style={styles.infoCardBodyText}>
                     The Next Step in Solving Today and Tomorrow’s Problems
                     Enabling individuals to log their location trail offers new opportunities for researchers studying pandemic tracking, refugee migration, and community traffic analysis.</Text>
-<Text style={{fontFamily: "OpenSans-Bold",
-  fontSize: 14,
-  lineHeight: 21,
-  letterSpacing: 0,
-  textAlign: "center",
-  color: "#ffffff",maxWidth:'84%',alignSelf:'center',marginTop:12}}>
+<Text onPress={() => Linking.openURL('https://privatekit.mit.edu')} style={[styles.infoCardBodyText,{fontFamily:'OpenSans-Bold',opacity:1,marginTop:12}]}>
 Learn More
 http://privatekit.mit.edu/
 </Text>
@@ -44,80 +22,30 @@ http://privatekit.mit.edu/
                 
             </View>
            
-           <View style={{flexDirection:'row',left:width*.445,marginTop:30}}>
+           <View style={styles.navigationDotsView}>
            <TouchableOpacity 
            onPress={()=>props.navigation.navigate('Intro1')}
-               style={{
-                width: 8,
-                height: 8,
-                borderRadius: 13,
-                backgroundColor: "#78849e",
-                opacity:.32,
-                marginRight:8
-            }}/>
+               style={styles.inactiveIndicator}/>
               
             <TouchableOpacity 
             onPress={()=>props.navigation.navigate('Intro2')}
-               style={{
-                width: 8,
-                height: 8,
-                opacity: 0.32,
-                borderRadius: 13,
-                backgroundColor: "#78849e",
-                marginRight:8
-
-            }}/>
+               style={styles.inactiveIndicator}/>
              <View 
-               style={{
-                width: 8,
-                height: 8,
-                opacity: 1,
-                borderRadius: 13,
-                backgroundColor: "#665EFF",
-            }}/>
+               style={styles.activeIndicator}/>
            </View>
 
-            <View style={{flexDirection:'row',justifyContent:'space-between',width:width*.7866,alignSelf:'center'}}>
+            <View style={styles.buttonContainer}>
             <TouchableOpacity 
             onPress={()=>props.navigation.navigate('Intro2')}
-            style={{
-                borderRadius: 12,
-                backgroundColor: "rgba(120, 132, 158, 0.16)",
-                height:52,
-                alignSelf:'center',
-                width:width*.38,
-                marginTop:30,
-                justifyContent:'center'
-                }} >
-                <Text 
-                    style={{
-                        fontFamily: "OpenSans-Bold",
-                        fontSize: 14,
-                        lineHeight: 19,
-                        letterSpacing: 0,
-                        textAlign: "center",
-                        color: "#454f63"}}>BACK</Text>
+            style={styles.secondaryButtonTouchable} >
+                <Text style={styles.secondaryButtonText}>BACK</Text>
             </TouchableOpacity>
           
            <TouchableOpacity 
            onPress={()=>props.navigation.navigate('LocationTrackingScreen')}
-            style={{
-                borderRadius: 12,
-                backgroundColor: "#665eff",
-                height:52,
-                alignSelf:'center',
-                width:width*.38,
-                marginTop:30,
-                justifyContent:'center'
-                }} >
+            style={styles.primaryButtonTouchable} >
                 <Text 
-                    style={{
-                        fontFamily: "OpenSans-Bold",
-                        fontSize: 14,
-                        lineHeight: 19,
-                        letterSpacing: 0,
-                        textAlign: "center",
-                        color: "#ffffff"}}>NEXT</Text>
+                    style={styles.primaryButtonText}>NEXT</Text>
             </TouchableOpacity>
             </View>
           
@@ -125,5 +53,85 @@ http://privatekit.mit.edu/
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    
+    mainContainer:{
+        flex:1,
+        backgroundColor:'white'
+    },
+    infoCard: {width:width*0.7866,backgroundColor:'#3497fc',height:'70%',borderRadius:12,alignSelf:'center',marginTop:'9%',justifyContent:'center'},
+    infoCardImage:{alignSelf:'center',width:width*.5,height:width*.5,marginTop:'16%'},
+    infoCardHeadText:{
+        fontFamily: "OpenSans-Bold",
+        fontSize: 24,
+        lineHeight: 55,
+        letterSpacing: 0,
+        textAlign: "center",
+        color: "#ffffff"
+        },
+        infoCardBodyText:{
+            opacity: 0.8,
+            fontFamily: "OpenSans-SemiBold",
+            fontSize: 14,
+            lineHeight: 24,
+            letterSpacing: 0,
+            textAlign: "center",
+            color: "#ffffff",
+            maxWidth:'84%',
+            alignSelf:'center',
+            marginTop:20},
+    navigationDotsView:{flexDirection:'row',left:width*.445,marginTop:30},
+    activeIndicator:{
+        width: 8,
+        height: 8,
+        borderRadius: 13,
+        backgroundColor: "#665EFF",
+        opacity:1,
+        marginRight:8
+    },
+    inactiveIndicator:{
+        width: 8,
+        height: 8,
+        opacity: 0.32,
+        borderRadius: 13,
+        backgroundColor: "#78849e",
+        marginRight:8
+    },
+    primaryButtonTouchable:{
+        borderRadius: 12,
+        backgroundColor: "#665eff",
+        height:52,
+        alignSelf:'center',
+        width:width*.38,
+        marginTop:30,
+        justifyContent:'center'
+        },
+        primaryButtonText:{
+            fontFamily: "OpenSans-Bold",
+            fontSize: 14,
+            lineHeight: 19,
+            letterSpacing: 0,
+            textAlign: "center",
+            color: "#ffffff"},
+    buttonContainer:{flexDirection:'row',justifyContent:'space-between',width:width*.7866,alignSelf:'center'},
+    secondaryButtonTouchable:{
+        borderRadius: 12,
+        backgroundColor: "rgba(120, 132, 158, 0.16)",
+        height:52,
+        alignSelf:'center',
+        width:width*.38,
+        marginTop:30,
+        justifyContent:'center'
+        },
+        secondaryButtonText:{
+            fontFamily: "OpenSans-Bold",
+            fontSize: 14,
+            lineHeight: 19,
+            letterSpacing: 0,
+            textAlign: "center",
+            color: "#454f63"}
+});
 
 export default Intro2
