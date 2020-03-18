ca9f6c7d8e11419c5dddd0108f6fd512dd00824a
Harsh Vitra <jainharsh44@gmail.com>
2020-03-18 23:23:21 +0530

    Feature/new ui (#81)
    * UI changed, added Welcome Screens, changed location tracking screen
    
    * Moved Styles
    
    * Slider for welcome screens
    
    * Navigate through pagination
    
    * Set initial route
    
    * SafeAreaView for ios
    
    * Added image
    
    * Set state to true
    
    * Dot navigation fixed
    
    * Android backhandler
    
    * UI Designed for Export and Import Pages
    
    * Latest news page UI
    
    Co-authored-by: Harsh Vitra <harshvitra@gmail.com>

--- app/views/LocationTracking.js
+++ app/views/LocationTracking.js
@@ -6,19 +6,58 @@ import {
     StyleSheet,
     Linking,
     View,
-    Text
+    Text,
+    TouchableOpacity,
+    Dimensions,
+    Image,
+    ScrollView,BackHandler
 } from 'react-native';
-
 import colors from "../constants/colors";
-import Button from "../components/Button";
 import LocationServices from '../services/LocationService';
+import exportImage from './../assets/images/export.png';
+import news from './../assets/images/newspaper.png';
+
+import pkLogo from './../assets/images/PKLogo.png';
+
+import {GetStoreData, SetStoreData} from '../helpers/General';
+
+const width = Dimensions.get('window').width;
 
 class LocationTracking extends Component {
     constructor(props) {
         super(props);
-        LocationServices.start();
+        
+        this.state = {
+            isLogging:''
+        }
     }
 
+    componentDidMount() {
+        BackHandler.addEventListener("hardwareBackPress", this.handleBackPress); 
+        GetStoreData('PARTICIPATE')
+        .then(isParticipating => {
+            console.log(isParticipating);
+               
+                if(isParticipating === 'true'){
+                    this.setState({
+                        isLogging:true
+                    })
+                    this.willParticipate()
+                }
+                else{
+                    this.setState({
+                        isLogging:false
+                    }) 
+                }
+        })
+        .catch(error => console.log(error))
+    }
+    componentWillUnmount() {     BackHandler.removeEventListener("hardwareBackPress", this.handleBackPress);   }   
+
+    handleBackPress = () => {     
+        BackHandler.exitApp(); // works best when the goBack is async     
+        return true;   
+    };   
     export() {
         this.props.navigation.navigate('ExportScreen', {})
     }
@@ -31,43 +70,81 @@ class LocationTracking extends Component {
         this.props.navigation.navigate('NewsScreen', {})
     }
 
+    willParticipate =()=> {
+        SetStoreData('PARTICIPATE', 'true').then(() =>
+            LocationServices.start()
+        );
+        this.setState({
+            isLogging:true
+        })
+    }
+
+    setOptOut =()=>{
+        LocationServices.optOut(this.props.navigation)
+        this.setState({
+            isLogging:false
+        })
+    }
+
     render() {
         return (
             <SafeAreaView style={styles.container} >
 
-                <View style={styles.main}>
+                <ScrollView contentContainerStyle={styles.main}>
                     <View style={styles.topView}>
                         <View style={styles.intro} >
 
                             <Text style={styles.headerTitle}>Private Kit</Text>
 
-                            <Text style={styles.sectionDescription}>Private Kit is your personal vault that nobody else can access.</Text>
-                            <Text style={styles.sectionDescription}>It is currently logging your location privately every five minutes. Your location information will NOT leave your phone.</Text>
+                            {
+                                this.state.isLogging  ? (
+                                    <>
+                                    <Image source={pkLogo} style={{width:132,height:164.4,alignSelf:'center',marginTop:12}} />
+
+                                <TouchableOpacity onPress={() => this.setOptOut()} style={styles.stopLoggingButtonTouchable} >
+                                <Text style={styles.stopLoggingButtonText}>STOP LOGGING</Text>
+                            </TouchableOpacity>
+                            </>
+                            ) : ( 
+                            <>
+                            <Image source={pkLogo} style={{width:132,height:164.4,alignSelf:'center',marginTop:12,opacity:.3}} />
+                            <TouchableOpacity onPress={() => this.willParticipate()} style={styles.startLoggingButtonTouchable} >
+                                <Text style={styles.startLoggingButtonText}>START LOGGING</Text>
+                            </TouchableOpacity>
+                            </>)
+                            }
+                           
+                           {this.state.isLogging ?  
+                            <Text style={styles.sectionDescription}>It is currently logging your location privately every five minutes. Your location information will NOT leave your phone.</Text> :
+                           <Text style={styles.sectionDescription} >NOTE: After clicking this button you may be prompted to grant Private Kit access to your location.</Text> }
+                           
+
+                            {/* <Text style={styles.sectionDescription}>Private Kit is your personal vault that nobody else can access.</Text> */}
 
                         </View>
                     </View>
 
-                    <View style={styles.block}>
-                        <Button title={"Stop Recording Location"} bgColor={colors.NEG_BUTTON} onPress={() => LocationServices.optOut(this.props.navigation)} />
-                    </View>
+                    <View style={styles.actionButtonsView}>
+                        <TouchableOpacity onPress={() => this.import()}  style={styles.actionButtonsTouchable}>
+                            <Image style={styles.actionButtonImage} source={exportImage} resizeMode={'contain'}></Image>
+                            <Text style={styles.actionButtonText}>Import</Text>
+                        </TouchableOpacity>
 
-                    <View style={styles.block}>
-                        <Button title={"News"} bgColor={colors.POS_BUTTON} onPress={() => this.news()} />
-                    </View>
+                        <TouchableOpacity onPress={() => this.export()} style={styles.actionButtonsTouchable}>
+                            <Image style={[styles.actionButtonImage,{transform:[{rotate:'180deg'}]}]} source={exportImage} resizeMode={'contain'}></Image>
+                            <Text style={styles.actionButtonText}>Export</Text>
+                        </TouchableOpacity>
 
-                    <View style={styles.block}>
-                        <Button title={"Import"} bgColor={colors.SENS_BUTTON} onPress={() => this.import()} />
+                        <TouchableOpacity onPress={() => this.news()} style={styles.actionButtonsTouchable}>
+                            <Image style={styles.actionButtonImage} source={news} resizeMode={'contain'}></Image>
+                            <Text style={styles.actionButtonText}>News</Text>
+                        </TouchableOpacity>
                     </View>
-
-                    <View style={styles.block}>
-                        <Button title={"Export"} bgColor={colors.SENS_BUTTON} onPress={() => this.export()} />
-                    </View>
-
-                </View>
+                </ScrollView>
 
                 <View style={styles.footer}>
-                    <Text style={styles.sectionDescription, { textAlign: 'center', paddingTop: 15 }}>For more information visit the Private Kit hompage:</Text>
-                    <Text style={styles.sectionDescription, { color: 'blue', textAlign: 'center' }} onPress={() => Linking.openURL('https://privatekit.mit.edu')}>privatekit.mit.edu</Text>
+                    <Text style={[styles.sectionDescription, { textAlign: 'center', paddingTop: 15 }]}>For more information visit the Private Kit hompage:</Text>
+                    <Text style={[styles.sectionDescription, { color: 'blue', textAlign: 'center',marginTop:0 }]} onPress={() => Linking.openURL('https://privatekit.mit.edu')}>privatekit.mit.edu</Text>
                 </View>
             </SafeAreaView>
         )
@@ -82,13 +159,13 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         color: colors.PRIMARY_TEXT,
-        backgroundColor: colors.APP_BACKGROUND,
+        backgroundColor: colors.WHITE,
     },
     headerTitle: {
         textAlign: 'center',
-        fontWeight: "bold",
         fontSize: 38,
-        padding: 0
+        padding: 0,
+        fontFamily:'OpenSans-Bold'
     },
     subHeaderTitle: {
         textAlign: 'center',
@@ -107,9 +184,6 @@ const styles = StyleSheet.create({
         margin: 20,
         width: "100%"
     },
-    topView: {
-        flex: 1,
-    },
     footer: {
         textAlign: 'center',
         fontSize: 12,
@@ -118,18 +192,79 @@ const styles = StyleSheet.create({
         paddingBottom: 10
     },
     intro: {
-        flex: 1,
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'stretch',
     },
     sectionDescription: {
-        fontSize: 18,
+        fontSize: 12,
         lineHeight: 24,
-        fontWeight: '400',
+        fontFamily:'OpenSans-Regular',
         marginTop: 20,
         marginLeft: 10,
         marginRight: 10
+    },
+    startLoggingButtonTouchable:{
+        borderRadius: 12,
+        backgroundColor: "#665eff",
+        height:52,
+        alignSelf:'center',
+        width:width*.7866,
+        marginTop:30,
+        justifyContent:'center'
+    },
+    startLoggingButtonText:{
+        fontFamily: "OpenSans-Bold",
+        fontSize: 14,
+        lineHeight: 19,
+        letterSpacing: 0,
+        textAlign: "center",
+        color: "#ffffff"
+    },
+    stopLoggingButtonTouchable:{
+        borderRadius: 12,
+        backgroundColor: "#fd4a4a",
+        height:52,
+        alignSelf:'center',
+        width:width*.7866,
+        marginTop:30,
+        justifyContent:'center',
+    },
+    stopLoggingButtonText:{
+        fontFamily: "OpenSans-Bold",
+        fontSize: 14,
+        lineHeight: 19,
+        letterSpacing: 0,
+        textAlign: "center",
+        color: "#ffffff"
+    },
+    actionButtonsView:{
+        width:width*.7866,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        marginTop:64
+    },
+    actionButtonsTouchable:{
+        height: 76,
+        borderRadius: 8,
+        backgroundColor: "#454f63",
+        width:width*.23,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    actionButtonImage:{
+        height:21.6,
+        width:32.2
+    },
+    actionButtonText:{
+        opacity: 0.56,
+        fontFamily: "OpenSans-Bold",
+        fontSize: 12,
+        lineHeight: 17,
+        letterSpacing: 0,
+        textAlign: "center",
+        color: "#ffffff",
+        marginTop:6
     }
 });
 
