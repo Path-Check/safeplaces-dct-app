4946c25bc7fbbf08de39f37ebb254cde399a1e7d
Abhishek Singh <abhi24@mit.edu>
2020-03-21 11:12:39 -0400

    working v2; only android

--- app/views/Overlap.js
+++ app/views/Overlap.js
@@ -27,18 +27,23 @@ import RNFetchBlob from 'rn-fetch-blob';
 import LocationServices from '../services/LocationService';
 import backArrow from './../assets/images/backArrow.png'
 import languages from './../locales/languages'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Marker, Circle, PROVIDER_GOOGLE } from 'react-native-maps';
 
 
 const width = Dimensions.get('window').width;
 
 const base64 = RNFetchBlob.base64
 
+const public_data = "https://docs.google.com/spreadsheets/d/1itaohdPiAeniCXNlntNztZ_oRvjh0HsGuJXUJWET008/export?format=csv"
 
 
 class OverlapScreen extends Component {
     constructor(props) {
         super(props);
+        //this.setState({region: {}});
+        this.state = {'region': {}, 'markers': [], 'circles': []};
+        this.getInitialState();
+        this.set_markers();
     }
 
     getOverlap = async () => {
@@ -49,24 +54,146 @@ class OverlapScreen extends Component {
         }
     };
 
-    getCoords () {
+    set_markers = async() => {
         GetStoreData('LOCATION_DATA')
         .then(locationArrayString => {
-
             var locationArray = JSON.parse(locationArrayString);
-            lastCoords = locationArray[locationArray.length - 1];
-            return lastCoords["latitude"], lastCoords["longitude"]
+            for (var i = 0; i < locationArray.length - 1; i+=50) {
+                console.log(i);
+                const coord = locationArray[i];
+                this.setState({
+                    markers : [
+                        ...this.state.markers,
+                        {
+                            coordinate: {
+                                latitude: coord["latitude"],
+                                longitude: coord["longitude"],
+                            },
+                            key: i + 1,
+                            color: '#f26964',
+                        }
+                    ],
+                });
+            }
         });
     };
 
-    displayMap = async () => {
+    getInitialState = async () => {
+        try { 
+            //const locationArray
+            GetStoreData('LOCATION_DATA')
+            .then(locationArrayString => {
+                var locationArray = JSON.parse(locationArrayString);
+                var lastCoords = locationArray[locationArray.length - 1];
+                this.setState({
+                    region: {
+                        latitude: lastCoords["latitude"],
+                        longitude: lastCoords["longitude"],
+                        latitudeDelta: 1.10922,
+                        longitudeDelta: 1.20421,
+                    },
+                    markers: [{coordinate: {
+                                latitude: lastCoords["latitude"],
+                                longitude: lastCoords["longitude"],
+                              },
+                              key: 0,
+                              color: '#f26964',
+                        },],
+                });
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    downloadAndPlot = async () => {
+        // Downloads the file on the disk and loads it into memory
         try {
-            console.log("test");
-            console.log(getCoords());
-            openMap({ latitude: 37.865101, longitude: -119.538330 });
+            // statements
+            RNFetchBlob
+              .config({
+                // add this option that makes response data to be stored as a file,
+                // this is much more performant.
+                fileCache : true,
+              })
+              .fetch('GET', public_data, {
+                //some headers ..
+              })
+              .then((res) => {
+                // the temp file path
+                console.log('The file saved to ', res.path())
+                try {
+                    RNFetchBlob.fs.readFile(res.path(), 'utf8')
+                      .then((records) => {
+                        this.parseCSV(records)
+                        .then((parsedRecords) =>{
+                            console.log(parsedRecords);
+                            this.plotCircles(parsedRecords);
+                        });
+                      })
+                      .catch((e) => {
+                        console.error("got error: ", e);
+                      })
+                  } catch (err) {
+                    console.log('ERROR:', err);
+                  }
+              })
+              //Load csv
+              //Convert to json
+              //Remove 
+        } catch(e) {
+            // statements
+            console.log(e);
+        }
+    }
+
+    parseCSV = async (records) => {
+        try {
+              var rows = records.split('\n');
+              var parsedRows = []
+              for (var i = rows.length - 1; i >= 0; i--) {
+                  var row = rows[i].split(',');
+                  if (row[7].length > 0 && row[8].length > 0) {
+                    parsedRows.push([parseFloat(row[7]), parseFloat(row[8])]);
+                  }
+              }
+              return parsedRows;
+        } catch(e) {
+            // statements
+            console.log(e);
+        }
+    };
+
+    plotCircles = async (records) => {
+        try {
+            console.log("length is ", records.length);
+            for (var i = records.length - 1; i >= 0; i-=100) {
+                var latitude = records[i][0];
+                var longitude = records[i][1];
+                if (!isNaN(latitude) && !isNaN(longitude)) {
+                    this.setState({
+                    circles : [
+                        ...this.state.circles,
+                        {
+                            center: {
+                                latitude: latitude,
+                                longitude: longitude,
+                            },
+                            radius: 7000
+                        }
+                    ],
+                    });
+                }
+            }
+            console.log("done!");
         } catch(e) {
             console.log(e);
         }
+    };
+
+    onRegionChange = async(region) => {
+        //console.log(this.setState());
+        //this.setState({region});
     };
 
     backToMain() {
@@ -87,7 +214,6 @@ class OverlapScreen extends Component {
     }
 
     render() {
-
         return (
             <SafeAreaView style={styles.container}>
 
@@ -100,14 +226,31 @@ class OverlapScreen extends Component {
                     <MapView
                         provider={PROVIDER_GOOGLE}
                         style={styles.main}
-                        initialRegion={{
-                          latitude: 37.78825,
-                          longitude: -122.4324,
-                          latitudeDelta: 0.0922,
-                          longitudeDelta: 0.0421,
-                        }}
-                    />
+                        region={this.state.region}
+                    >
+                        {this.state.markers.map(marker => (
+                        <Marker
+                          coordinate={marker.coordinate}
+                          title={marker.title}
+                          description={marker.description}
+                        />
+                        ))}
+
+                        {this.state.circles.map(circle => (
+                        <Circle
+                            center={circle.center}
+                            radius={circle.radius}
+                            fillColor="rgba(208, 35, 35, 0.3)"
+                            strokeColor="rgba(0,0,0,0.5)"
+                            zIndex={2}
+                            strokeWidth={2}
+                        />
+                        ))}
+                    </MapView>
                 <View style={styles.main}>
+                    <TouchableOpacity style={styles.buttonTouchable} onPress={this.downloadAndPlot}>
+                        <Text style={styles.buttonText}>{languages.t('label.show_overlap')}</Text>
+                    </TouchableOpacity>
                     <Text style={styles.sectionDescription}>{languages.t('label.overlap_para_1')}</Text>
                 </View>
 
