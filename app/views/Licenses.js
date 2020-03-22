d1411535ae57125f7d24014ca70d4476c3af6733
Sam Stowers <phantom4d@gmail.com>
2020-03-22 00:43:40 -0500

    Add modal menu for licenses to main screen

---
+++ app/views/Licenses.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  BackHandler,
+} from 'react-native';
+
+import colors from '../constants/colors';
+import WebView from 'react-native-webview';
+import Button from '../components/Button';
+import { GetStoreData } from '../helpers/General';
+import { convertPointsToString } from '../helpers/convertPointsToString';
+import Share from 'react-native-share';
+import RNFetchBlob from 'rn-fetch-blob';
+import LocationServices from '../services/LocationService';
+import backArrow from './../assets/images/backArrow.png';
+import languages from './../locales/languages';
+
+const width = Dimensions.get('window').width;
+
+const base64 = RNFetchBlob.base64;
+
+class LicensesScreen extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  backToMain() {
+    this.props.navigation.navigate('LocationTrackingScreen', {});
+  }
+
+  handleBackPress = () => {
+    this.props.navigation.navigate('LocationTrackingScreen', {});
+    return true;
+  };
+
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.handleBackPress);
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.headerContainer}>
+          <TouchableOpacity
+            style={styles.backArrowTouchable}
+            onPress={() => this.backToMain()}>
+            <Image style={styles.backArrow} source={backArrow} />
+          </TouchableOpacity>
+          <Text style={styles.headerTitle}>Licenses</Text>
+        </View>
+
+        <View style={styles.main}>
+          <Text style={styles.sectionDescription}>
+            {/* This screen is a placeholder for complete license content, or a link */}
+            {languages.t('label.license_placeholder')}
+          </Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  // Container covers the entire screen
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    color: colors.PRIMARY_TEXT,
+    backgroundColor: colors.WHITE,
+  },
+  headerTitle: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 38,
+    padding: 0,
+  },
+  subHeaderTitle: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 22,
+    padding: 5,
+  },
+  main: {
+    flex: 1,
+    flexDirection: 'column',
+    textAlignVertical: 'top',
+    // alignItems: 'center',
+    padding: 20,
+    width: '96%',
+    alignSelf: 'center',
+  },
+  buttonTouchable: {
+    borderRadius: 12,
+    backgroundColor: '#665eff',
+    height: 52,
+    alignSelf: 'center',
+    width: width * 0.7866,
+    marginTop: 30,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontFamily: 'OpenSans-Bold',
+    fontSize: 14,
+    lineHeight: 19,
+    letterSpacing: 0,
+    textAlign: 'center',
+    color: '#ffffff',
+  },
+  mainText: {
+    fontSize: 18,
+    lineHeight: 24,
+    fontWeight: '400',
+    textAlignVertical: 'center',
+    padding: 20,
+  },
+  smallText: {
+    fontSize: 10,
+    lineHeight: 24,
+    fontWeight: '400',
+    textAlignVertical: 'center',
+    padding: 20,
+  },
+
+  headerContainer: {
+    flexDirection: 'row',
+    height: 60,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(189, 195, 199,0.6)',
+    alignItems: 'center',
+  },
+  backArrowTouchable: {
+    width: 60,
+    height: 60,
+    paddingTop: 21,
+    paddingLeft: 20,
+  },
+  backArrow: {
+    height: 18,
+    width: 18.48,
+  },
+  sectionDescription: {
+    fontSize: 16,
+    lineHeight: 24,
+    marginTop: 12,
+    fontFamily: 'OpenSans-Regular',
+  },
+});
+
+export default LicensesScreen;
