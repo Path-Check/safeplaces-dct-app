835e9ba1047890044b3ed7d6732c78dc6b20f59e
Niels de Hoog <njdehoog@gmail.com>
2020-03-24 03:59:55 +0100

    Add NL (Dutch) locale (#172)

---
+++ app/locales/nl/index.js
@@ -0,0 +1,13 @@
+import intro from './intro.json';
+import locationTracking from './locationTracking.json';
+import importFile from './import.json';
+import exportFile from './exportscreen.json';
+import licensesFile from './licensesscreen.json';
+
+export default {
+  ...intro,
+  ...locationTracking,
+  ...importFile,
+  ...exportFile,
+  ...licensesFile,
+};
