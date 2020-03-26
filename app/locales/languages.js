3ad0ad95ddb2f15ff6ad73dc7d6cedf8ecd647d3
Roman Pospisil <roman.pospisil@cmit.cz>
2020-03-26 12:07:48 +0000

    CS (Czech) locale
    This translation was created by myself. I am native speaker.

--- app/locales/languages.js
+++ app/locales/languages.js
@@ -17,6 +17,7 @@ import itlabels from './it';
 import ptlabels from './pt';
 import mrlabels from './mr';
 import nllabels from './nl';
+import cslabels from './cs';
 
 // This will fetch the user's language
 let userLang = undefined;
@@ -71,6 +72,11 @@ i18next.init({
     nl: {
       translation: {
         label: nllabels,
+      },
+    },
+	cs: {
+      translation: {
+        label: cslabels,
       },
     },
   },
