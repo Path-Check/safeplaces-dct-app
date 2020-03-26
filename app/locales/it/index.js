bb2c908da8401c9ba607d5e093d6b090f717b6b4
Andy Nuzzo <36167533+andreanuzzo@users.noreply.github.com>
2020-03-26 17:50:28 -0400

    Update index.js

--- app/locales/it/index.js
+++ app/locales/it/index.js
@@ -2,7 +2,6 @@ import intro from './intro.json';
 import locationTracking from './locationTracking.json';
 import importFile from './import.json';
 import exportFile from './exportscreen.json';
-import licensesFile from './licensesscreen.json';
 import overlapFile from './overlap.json';
 
 export default {
