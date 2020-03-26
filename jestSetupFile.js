7374b544e2111f3741909dca092040e9d5b15dbe
Tim Banks <thetimbanks@gmail.com>
2020-03-26 10:00:03 -0500

    Update tests after rebasing the develop branch

--- jestSetupFile.js
+++ jestSetupFile.js
@@ -5,6 +5,7 @@ jest.mock('@react-native-community/push-notification-ios', () => 'push-notificat
 jest.mock('react-native-share', () => 'Share');
 jest.mock('rn-fetch-blob', () => 'Blob');
 jest.mock('react-native-map-clustering', () => 'MapView');
+jest.mock('react-native-background-timer', () => 'BackgroundTimer');
 jest.mock('react-native-popup-menu', () => ({
   Menu: 'Menu',
   MenuProvider: 'MenuProvider',
