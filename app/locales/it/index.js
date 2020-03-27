df02c399646890a5da8fd77e8d14306de348af71
alpita-masurkar <alpita-masurkar@users.noreply.github.com>
2020-03-27 17:50:18 +0100

    Italian missing licenses file- Andy's translation added (#241)
    * Andy's translation for the missing licenses file
    
    * Update index.js

--- app/locales/it/index.js
+++ app/locales/it/index.js
@@ -2,6 +2,7 @@ import intro from './intro.json';
 import locationTracking from './locationTracking.json';
 import importFile from './import.json';
 import exportFile from './exportscreen.json';
+import licensesFile from './licensesscreen.json';
 import overlapFile from './overlap.json';
 
 export default {
