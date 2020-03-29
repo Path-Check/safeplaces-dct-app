ae5d6643f2b79477a10c522e77fc482cf28879f8
kalidasa <kali_in@yahoo.com>
2020-03-29 22:39:04 +0530

    Adding Kannada Language support
    Adding Kannada Language support

--- app/locales/languages.js
+++ app/locales/languages.js
@@ -20,6 +20,7 @@ import nllabels from './nl';
 import htlabels from './ht';
 import pt_BRlabels from './pt_BR';
 import eslabels from './es';
+import knlabels from './kn';
 
 // This will fetch the user's language
 let userLang = undefined;
@@ -84,6 +85,11 @@ i18next.init({
     pt_BR: {
       translation: {
         label: pt_BRlabels,
+      },
+    },
+    kn: {
+      translation: {
+        label: knlabels,
       },
     },
     es: {
