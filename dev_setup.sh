b7a2807b59f1bd2ca04edad9b4c5adddc6269223
Tim Stirrat <tstirrat@google.com>
2020-03-29 16:43:43 -0700

    Allow setting env vars on mac, with clearer instructions

--- dev_setup.sh
+++ dev_setup.sh
@@ -136,18 +136,13 @@ fi
 # Need Android Studio (interactive for now)
 if ! found_exe android-studio ; then
 
-    echo "Building for Android requires Android Studio.  Would you like to install it?"
+    echo "Building for Android requires Android Studio.  Would you like to see Android Studio instructions?"
     if get_YN "" "" "Skipping" ; then
 
         echo "${YELLOW}Install Android Studio from https://developer.android.com/studio/index.html${RESET}"
         echo "You can also use your software store to install."
         echo "${BLUE}Press RETURN after completing this step.${RESET}"
         read
-
-        if ! found_exe android-studio ; then
-            echo "Not able to find Android Studio still, aborting."
-            exit 1
-        fi
 
         # Need Android 9 (Pie) SDK
         echo "We need the Android 9 (Pie) SDK.  Please follow these steps:"
@@ -166,16 +161,23 @@ if ! found_exe android-studio ; then
         echo ""
         echo "${BLUE}Press RETURN after completing these steps.${RESET}"
 
-        echo "${BLUE}Adding environment variables via ~/.profile_mobileapp${RESET}"
+        echo "${BLUE}Adding environment variables to ${YELLOW}~/.profile_mobileapp${RESET}"
 
-        echo "# ==== Added by PrivateKit/mobileapp's dev_setup.sh ====" >> ~/.profile_mobileapp
-        echo "export ANDROID_HOME=\$HOME/Android/Sdk" >> ~/.profile_mobileapp
-        echo "export PATH=\$PATH:\$ANDROID_HOME/emulator" >> ~/.profile_mobileapp
-        echo "export PATH=\$PATH:\$ANDROID_HOME/tools" >> ~/.profile_mobileapp
-        echo "export PATH=\$PATH:\$ANDROID_HOME/tools/bin" >> ~/.profile_mobileapp
-        echo "export PATH=\$PATH:\$ANDROID_HOME/platform-tools" >> ~/.profile_mobileapp
+        echo "# ==== Added by PrivateKit/mobileapp's dev_setup.sh ====" > ~/.profile_mobileapp
+        if [[ "$OSTYPE" == "darwin"* ]] ; then
+            echo "export ANDROID_SDK_ROOT=\$HOME/Library/Android/sdk" >> ~/.profile_mobileapp
+        else
+            echo "export ANDROID_SDK_ROOT=\$HOME/Android/Sdk" >> ~/.profile_mobileapp
+        fi
+        echo "export PATH=\$PATH:\$ANDROID_SDK_ROOT/emulator" >> ~/.profile_mobileapp
+        echo "export PATH=\$PATH:\$ANDROID_SDK_ROOT/tools" >> ~/.profile_mobileapp
+        echo "export PATH=\$PATH:\$ANDROID_SDK_ROOT/tools/bin" >> ~/.profile_mobileapp
+        echo "export PATH=\$PATH:\$ANDROID_SDK_ROOT/platform-tools" >> ~/.profile_mobileapp
 
-        echo "source ~/.profile_mobileapp" >> ~/.profile
+        echo "Would you like to source environment variables in ${YELLOW}~/.profile${RESET}?"
+        if get_YN "" "" "Skipping" ; then
+            echo "source ~/.profile_mobileapp" >> ~/.profile
+        fi
 
         echo "${GREEN}Android Studio installed!${RESET}"
         echo "${YELLOW}You will need to start a new terminal session for this to apply.${RESET}"
@@ -215,6 +217,9 @@ echo
 echo "  Run Android Studio then:"
 echo "    * Select 'Configure > AVD Manager' at the bottom"
 echo "    * Create and run a virtual device, such as a Pixel 2"
+echo
+echo "  Configure Android environment in your ~/.bashrc (or equivalent):"
+echo "    source ~/.profile_mobileapp"
 echo
 echo "  In a terminal run:"
 echo "      $ ./1_start_react.sh"
