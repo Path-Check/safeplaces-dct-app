0b3e2899dbbcbe7018d6d3ed7ca381187df73966
heri <heri@studiozenkai.com>
2020-03-30 09:54:48 -0400

    Added translations for new keys + updates

--- app/locales/fr/index.js
+++ app/locales/fr/index.js
@@ -4,12 +4,14 @@ import importFile from './import.json';
 import exportFile from './exportscreen.json';
 import licensesFile from './licensesscreen.json';
 import overlapFile from './overlap.json';
+import notificationFile from './notification.json';
 
 export default {
-    ...intro,
-    ...locationTracking,
-    ...importFile,
-    ...exportFile,
-    ...overlapFile,
-    ...licensesFile,
+  ...intro,
+  ...locationTracking,
+  ...importFile,
+  ...exportFile,
+  ...overlapFile,
+  ...licensesFile,
+  ...notificationFile,
 };
