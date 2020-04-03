7e85c37c57ba4210d9c65b1c20c5196f616e73c0
Ken Pugsley <>
2020-04-03 18:39:24 -0500

    add async to Welcome.spec.js

--- app/views/__tests__/Welcome.spec.js
+++ app/views/__tests__/Welcome.spec.js
@@ -3,7 +3,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Welcome from '../Welcome';
 
-it('renders correctly', () => {
+it('renders correctly', async () => {
   const tree = renderer
     .create(<Welcome />)
     .toJSON();
