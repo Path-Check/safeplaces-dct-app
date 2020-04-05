78a8bd026d2af9ede811e4e243b6b5a5dff112f4
Tim Stirrat <tim.stirrat@gmail.com>
2020-04-04 18:57:35 -0700

    Fix black status bar on intermediate splash

--- android/app/src/main/java/edu/mit/privatekit/MainActivity.java
+++ android/app/src/main/java/edu/mit/privatekit/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this);
+    SplashScreen.show(this, R.style.SplashTheme);
     super.onCreate(savedInstanceState);
   }
 
