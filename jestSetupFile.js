c2f31d13f9a6b9301c53f603f5f0bc358f4c8a36
Tim Stirrat <tim.stirrat@gmail.com>
2020-04-06 13:05:20 -0700

    Silence YellowBox useNativeDriver warning

--- jestSetupFile.js
+++ jestSetupFile.js
@@ -1,5 +1,7 @@
 import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
 
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+
 jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
 jest.mock(
   '@react-native-community/push-notification-ios',
