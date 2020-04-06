112abf673962729c518df9e3794f66f69f697cc6
Tim Stirrat <tim.stirrat@gmail.com>
2020-04-06 13:27:02 -0700

    Remove inline splashscreen mock, use __mocks__ version

--- __tests__/App.spec.js
+++ __tests__/App.spec.js
@@ -8,9 +8,6 @@ import App from '../App';
 import {render} from '@testing-library/react-native';
 
 jest.mock('../app/Entry', () => 'Entry');
-jest.mock('react-native-splash-screen', () => ({
-  hide: jest.fn(),
-}));
 
 it('renders correctly', () => {
   const {asJSON} = render(<App />);
