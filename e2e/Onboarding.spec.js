e3ef9b3c0e6cd170dec5d441ca803f0289bd9abe
Tim Stirrat <tim.stirrat@gmail.com>
2020-04-09 10:13:59 -0700

    Create a meaningful test, update button

---
+++ e2e/Onboarding.spec.js
@@ -0,0 +1,25 @@
+describe('Onboarding', () => {
+  beforeEach(async () => {
+    console.log('reload');
+    await device.reloadReactNative();
+    console.log('reloaded');
+  });
+
+  it('starts at onboarding step 1', async () => {
+    await expect(element(by.label('Get Started'))).toBeVisible();
+  });
+
+  it('proceeds to step 2', async () => {
+    await element(by.label('Get Started')).tap();
+
+    await expect(
+      element(
+        by.text(
+          'Get notified if you cross paths with someone later diagnosed ' +
+            'for COVID-19.',
+        ),
+      ),
+    ).toBeVisible();
+    await expect(element(by.label('Next'))).toBeVisible();
+  });
+});
