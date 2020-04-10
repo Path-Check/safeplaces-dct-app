540256ca85e4dbbb96d167f52f34931f0b85ca36
Tim Stirrat <tim.stirrat@gmail.com>
2020-04-09 17:09:47 -0700

    change locale location

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -5,7 +5,7 @@ module.exports = {
   createOldCatalogs: true,
   // Save the \_old files
 
-  defaultNamespace: 'index',
+  defaultNamespace: 'default',
   // Default namespace used in your i18next config
 
   defaultValue: '',
@@ -62,7 +62,7 @@ module.exports = {
   // Namespace separator used in your translation keys
   // If you want to use plain english keys, separators such as `.` and `:` will conflict. You might want to set `keySeparator: false` and `namespaceSeparator: false`. That way, `t('Status: Loading...')` will not think that there are a namespace and three separator dots for instance.
 
-  output: 'app/locales/$LOCALE/$NAMESPACE.json',
+  output: 'app/locales/$LOCALE.json',
   // Supports $LOCALE and $NAMESPACE injection
   // Supports JSON (.json) and YAML (.yml) file formats
   // Where to write the locale files relative to process.cwd()
