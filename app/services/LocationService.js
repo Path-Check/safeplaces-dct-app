8a353ec299522b40c56ce68c7c2c01773fe949bf
Ken Pugsley <>
2020-04-10 01:23:18 -0500

    remove dead code

--- app/services/LocationService.js
+++ app/services/LocationService.js
@@ -53,28 +53,6 @@ export class LocationData {
     };
   }
 
-  /*
-   * Calculate fairly accurately the distance between to lat-long points
-   *    Uses the Spherical Law of Cosines method.
-   *    https://www.movable-type.co.uk/scripts/latlong.html
-   *    https://en.wikipedia.org/wiki/Spherical_law_of_cosines
-   *
-   * Calculates the distance in meters
-   */
-  calcDistance(lat1, lon1, lat2, lon2) {
-    var p1 = (lat1 * Math.PI) / 180;
-    var p2 = (lat2 * Math.PI) / 180;
-    var deltaLambda = ((lon2 - lon1) * Math.PI) / 180;
-    var R = 6371e3; // gives d in metres
-
-    var d =
-      Math.acos(
-        Math.sin(p1) * Math.sin(p2) +
-          Math.cos(p1) * Math.cos(p2) * Math.cos(deltaLambda),
-      ) * R;
-    return d;
-  }
-
   saveLocation(location) {
     // Persist this location data in our local storage of time/lat/lon values
     this.getLocationData().then(locationArray => {
