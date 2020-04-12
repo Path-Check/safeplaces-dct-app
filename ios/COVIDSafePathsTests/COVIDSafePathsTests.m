ee6d3c130f54492eaf1f10de07f01f40848473d2
kpugsley <ken.pugsley@gmail.com>
2020-04-12 12:16:46 -0500

    Final removal of Private Kit (leaving only attributions) (#465)
    * pass 1
    
    * pass 2
    
    * pass 3
    
    * rewording of the location and accelerometer permission descriptions.
    
    * remove eclipse specific files
    
    Co-authored-by: Ken Pugsley <>

--- ios/COVIDSafePathsTests/COVIDSafePathsTests.m
+++ ios/COVIDSafePathsTests/COVIDSafePathsTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface PrivateKitTests : XCTestCase
+@interface COVIDSafePathsTests : XCTestCase
 
 @end
 
-@implementation PrivateKitTests
+@implementation COVIDSafePathsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
