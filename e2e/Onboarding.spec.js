b8d2d7dd19d6a2ceb9f18c6e3a16ee7694b38cfe
Tim Stirrat <tim.stirrat@gmail.com>
2020-04-13 16:35:13 -0700

    Simplest test ever

--- e2e/Onboarding.spec.js
+++ e2e/Onboarding.spec.js
@@ -9,17 +9,17 @@ describe('Onboarding', () => {
     await expect(element(by.label('Get Started'))).toBeVisible();
   });
 
-  it('proceeds to step 2', async () => {
-    await element(by.label('Get Started')).tap();
+  // it('proceeds to step 2', async () => {
+  //   await element(by.label('Get Started')).tap();
 
-    await expect(
-      element(
-        by.text(
-          'Get notified if you cross paths with someone later diagnosed ' +
-            'for COVID-19.',
-        ),
-      ),
-    ).toBeVisible();
-    await expect(element(by.label('Next'))).toBeVisible();
-  });
+  //   await expect(
+  //     element(
+  //       by.text(
+  //         'Get notified if you cross paths with someone later diagnosed ' +
+  //           'for COVID-19.',
+  //       ),
+  //     ),
+  //   ).toBeVisible();
+  //   await expect(element(by.label('Next'))).toBeVisible();
+  // });
 });
