0ee2288336fee63915e1e922306c6d3d4ccf939f
alpita-masurkar <alpita-masurkar@users.noreply.github.com>
2020-04-13 23:50:45 +0200

    Spanish Translation from Miquel Vila Porte (#491)

--- app/locales/languages.js
+++ app/locales/languages.js
@@ -4,6 +4,7 @@ import { getLanguages } from 'react-native-i18n';
 import { LANG_OVERRIDE } from '../constants/storage';
 import { GetStoreData } from '../helpers/General';
 import en from './en.json';
+import es from './es.json';
 import ht from './ht.json';
 import it from './it.json';
 
@@ -56,8 +57,9 @@ i18next.init({
   fallbackLng: 'en', // If language detector fails
   resources: {
     en: { label: 'English', translation: en },
+    es: { label: 'Español', translation: es },
     ht: { label: 'Kreyòl ayisyen', translation: ht },
-    it: { label: 'Italian', translation: it },
+    it: { label: 'Italiano', translation: it },
   },
 });
 
