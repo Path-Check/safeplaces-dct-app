ae072b52a26fadc5382972cfe8aedfcb7026059b
Qingqing Liu <qingqing.liu@salesforce.com>
2020-04-16 17:03:14 -0700

    add auto format support

--- app/views/WayPoints.js
+++ app/views/WayPoints.js
@@ -1,15 +1,11 @@
 import Geolocation from '@react-native-community/geolocation';
 import React, { Component } from 'react';
-import {
-  BackHandler,
-  StyleSheet,
-  View,
-} from 'react-native';
-import MapView , { Circle, PROVIDER_DEFAULT, Polyline } from 'react-native-maps';
+import { BackHandler, StyleSheet, View } from 'react-native';
+import MapView, { Circle, PROVIDER_DEFAULT, Polyline } from 'react-native-maps';
 
 import languages from './../locales/languages';
 import NavigationBarWrapper from '../components/NavigationBarWrapper';
-import {WAYPOINTS_API} from '../constants/apis';
+import { WAYPOINTS_API } from '../constants/apis';
 import Colors from '../constants/colors';
 
 const InitialRegion = {
@@ -24,7 +20,7 @@ const InitialRegion = {
  */
 class WayPointsScreen extends Component {
   map = React.createRef();
-  
+
   constructor(props) {
     super(props);
     this.state = {
@@ -48,7 +44,10 @@ class WayPointsScreen extends Component {
     Geolocation.getCurrentPosition(
       position => {
         if (position && position.coords) {
-          this.getWayPoints(position.coords.latitude, position.coords.longitude);
+          this.getWayPoints(
+            position.coords.latitude,
+            position.coords.longitude,
+          );
         }
       },
       error => {
@@ -60,12 +59,12 @@ class WayPointsScreen extends Component {
   componentWillUnmount() {
     BackHandler.removeEventListener('hardwareBackPress', this.handleBackPress);
   }
-  
+
   async getWayPoints(latitude, longitude) {
-    const url = `${WAYPOINTS_API}?lat=${latitude}&lon=${longitude}&radius=50`
+    const url = `${WAYPOINTS_API}?lat=${latitude}&lon=${longitude}&radius=50`;
     const response = await fetch(url);
     const json = await response.json();
-    const fullLocationData = json.data
+    const fullLocationData = json.data;
 
     const locationDataForLine = [];
     const locationDataForCircle = [];
@@ -85,84 +84,88 @@ class WayPointsScreen extends Component {
         },
       });
     }
-    const region = this.getRegionForCoordinates(latitude, longitude, locationDataForLine)
+    const region = this.getRegionForCoordinates(
+      latitude,
+      longitude,
+      locationDataForLine,
+    );
     this.setState({
       region,
       locationDataForLine,
       locationDataForCircle,
     });
   }
-  
- getRegionForCoordinates(latitude, longitude, points) {
+
+  getRegionForCoordinates(latitude, longitude, points) {
     // points should be an array of { latitude: X, longitude: Y }
     let minX = latitude;
     let maxX = latitude;
     let minY = longitude;
     let maxY = longitude;
-  
+
     // calculate rect
-    points.map((point) => {
+    points.map(point => {
       minX = Math.min(minX, point.latitude);
       maxX = Math.max(maxX, point.latitude);
       minY = Math.min(minY, point.longitude);
       maxY = Math.max(maxY, point.longitude);
     });
-  
+
     // add default padding on delta
-    const defaultPadding = 0.01
+    const defaultPadding = 0.01;
     const midX = (minX + maxX) / 2;
     const midY = (minY + maxY) / 2;
-    let deltaX = (maxX - minX) + defaultPadding;
-    let deltaY = (maxY - minY) + defaultPadding;
-  
+    let deltaX = maxX - minX + defaultPadding;
+    let deltaY = maxY - minY + defaultPadding;
+
     return {
       latitude: midX,
       longitude: midY,
       latitudeDelta: deltaX,
-      longitudeDelta: deltaY
+      longitudeDelta: deltaY,
     };
   }
   renderMap() {
-    const {region, locationDataForLine, locationDataForCircle} = this.state;
+    const { region, locationDataForLine, locationDataForCircle } = this.state;
 
     return (
       <MapView
-      ref={ref => { this.map = ref }}
-      provider={PROVIDER_DEFAULT}
-      style={styles.map}
-      region={region}
-      loadingEnabled
-      showsUserLocation>
-      <Polyline
-        coordinates={locationDataForLine}
-        strokeColor={Colors.MAP_LINE_STROKE}
-        strokeWidth={1}
-      />
-      {locationDataForCircle.map(circle => (
-        <Circle
-          key={circle.key}
-          center={circle.center}
-          radius={20}
-          lineCap='round'
-          lineJoin='round'
-          strokeWidth={5}
+        ref={ref => {
+          this.map = ref;
+        }}
+        provider={PROVIDER_DEFAULT}
+        style={styles.map}
+        region={region}
+        loadingEnabled
+        showsUserLocation>
+        <Polyline
+          coordinates={locationDataForLine}
           strokeColor={Colors.MAP_LINE_STROKE}
-          fillColor={Colors.MAP_LINE_STROKE}
+          strokeWidth={1}
         />
-      ))}
-    </MapView>
+        {locationDataForCircle.map(circle => (
+          <Circle
+            key={circle.key}
+            center={circle.center}
+            radius={20}
+            lineCap='round'
+            lineJoin='round'
+            strokeWidth={5}
+            strokeColor={Colors.MAP_LINE_STROKE}
+            fillColor={Colors.MAP_LINE_STROKE}
+          />
+        ))}
+      </MapView>
     );
   }
-  
+
   render() {
-     return (
-        <NavigationBarWrapper
-          title={languages.t('waypoints.title')}
-          onBackPress={this.backToMain.bind(this)}>
-            <View style={styles.mainContainer}>
-              {this.renderMap()}
-            </View>
-        </NavigationBarWrapper>
+    return (
+      <NavigationBarWrapper
+        title={languages.t('waypoints.title')}
+        onBackPress={this.backToMain.bind(this)}>
+        <View style={styles.mainContainer}>{this.renderMap()}</View>
+      </NavigationBarWrapper>
     );
   }
 }
