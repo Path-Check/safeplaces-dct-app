7467eeaa602ef3e7538d32ab3271d1957b597e97
Abhishek Singh <abhishek.s14@iiits.in>
2020-04-18 20:30:17 -0400

    add screen for EULA

--- app/views/onboarding/Onboarding4.js
+++ app/views/onboarding/Onboarding4.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Dimensions,
   ImageBackground,
+  Modal,
   StatusBar,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 import BackgroundImage from './../../assets/images/launchScreen1.png';
 import ButtonWrapper from '../../components/ButtonWrapper';
@@ -17,6 +19,8 @@ import languages from '../../locales/languages';
 const width = Dimensions.get('window').width;
 
 const Onboarding = props => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [signedEula, setSignedEula] = useState(false);
   return (
     <View style={styles.mainContainer}>
       <StatusBar
@@ -24,6 +28,52 @@ const Onboarding = props => {
         backgroundColor='transparent'
         translucent={true}
       />
+      <View style={styles.centeredView}>
+        <Modal
+          animationType='slide'
+          transparent={true} // eslint-disable-line react/jsx-boolean-value
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(false);
+          }}>
+          <View
+            style={[
+              styles.overlay,
+              { flex: 1, alignItems: 'center', justifyContent: 'center' },
+            ]}>
+            <View style={styles.modalView}>
+              <ScrollView>
+                <Text style={styles.subheaderText}>
+                  {languages.t('label.launch_eula_title')}
+                </Text>
+                <Text style={styles.modalContent}>
+                  {languages.t('label.launch_eula_content')}
+                </Text>
+                <View style={styles.modalButtons}>
+                  <ButtonWrapper
+                    title={languages.t('label.launch_eula_not_agree')}
+                    onPress={() => {
+                      setSignedEula(false);
+                      setModalVisible(false);
+                    }}
+                    buttonColor={Colors.WHITE}
+                    bgColor={Colors.VIOLET_BUTTON}
+                  />
+                  <ButtonWrapper
+                    title={languages.t('label.launch_eula_agree')}
+                    onPress={() => {
+                      setSignedEula(true);
+                      setModalVisible(false);
+                    }}
+                    buttonColor={Colors.WHITE}
+                    bgColor={Colors.GREEN}
+                  />
+                </View>
+              </ScrollView>
+            </View>
+          </View>
+        </Modal>
+      </View>
       <ImageBackground
         source={BackgroundImage}
         style={styles.backgroundImage}
@@ -38,9 +88,15 @@ const Onboarding = props => {
       </View>
       <View style={styles.footerContainer}>
         <ButtonWrapper
-          title={languages.t('label.launch_set_up_phone')}
+          title={
+            signedEula
+              ? languages.t('label.launch_set_up_phone')
+              : languages.t('label.launch_sign_eula')
+          }
           onPress={() => {
-            props.navigation.replace('Onboarding5');
+            signedEula
+              ? props.navigation.replace('Onboarding5')
+              : setModalVisible(!modalVisible);
           }}
           buttonColor={Colors.WHITE}
           bgColor={Colors.VIOLET_BUTTON}
@@ -75,17 +131,60 @@ const styles = StyleSheet.create({
     fontFamily: fontFamily.primaryMedium,
   },
   subheaderText: {
+    marginTop: '10%',
+    color: Colors.VIOLET,
+    fontSize: 26,
+    //width: width * 0.6,
+    fontFamily: fontFamily.primaryRegular,
+    textAlign: 'center',
+  },
+  modalContent: {
     marginTop: '6%',
     color: Colors.VIOLET,
-    fontSize: 15,
+    fontSize: 20,
     width: width * 0.6,
     fontFamily: fontFamily.primaryRegular,
+  },
+  modalButtons: {
+    //flexDirection: 'row',
+    alignSelf: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 10,
   },
   footerContainer: {
     position: 'absolute',
     bottom: 0,
     marginBottom: '10%',
     alignSelf: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  modalView: {
+    marginTop: 40,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    backgroundColor: '#06273F80',
   },
 });
 
