91d1dbfac3011edac105a6b9f9f8ccb48f965fe8
Liz Falkner <elizabethfalkner09@gmail.com>
2020-04-20 19:11:01 -0400

    add a comment for the config file

--- react-native.config.js
+++ react-native.config.js
@@ -4,6 +4,8 @@ module.exports = {
     android: {},
   },
   assets: ['./app/assets/fonts'],
+  // add dependencies to this list when they require manual linking,
+  // so that the CLI does not return an error
   dependencies: {
     'react-native-reanimated': {
       platforms: {
