945b4f459cce2f580c69f4252c09c1fd59c77474
Ahmednagii <ahmed.nagi@extremesolution.com>
2020-04-21 04:26:07 +0200

    feature-rtl remove abstracted alert method

--- app/Util.js
+++ app/Util.js
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs';
-import { Platform, Alert } from 'react-native';
+import { Platform } from 'react-native';
 
 export function isPlatformiOS() {
   return Platform.OS === 'ios';
@@ -13,25 +13,8 @@ export function nowStr() {
   return dayjs().format('H:mm');
 }
 
-export function showAlert(title, description, onOkPress, okText, onCancelPress, cancelText) {
-  return Alert.alert(title, description,
-    [
-      {
-        text: okText,
-        onPress: onOkPress
-      },
-      {
-        text: cancelText,
-        onPress: onCancelPress,
-        style: 'cancel',
-      },
-    ],
-  )
-}
-
 export default {
   isPlatformiOS,
   isPlatformAndroid,
   nowStr,
-  showAlert
 };
