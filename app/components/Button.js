8deaa59eab6e674717d4bbf9b32c132a0cd28794
Tim Stirrat <tim.stirrat@gmail.com>
2020-04-22 16:54:13 -0700

    Add secondary button style

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,4 +1,5 @@
 import styled from '@emotion/native';
+import { ThemeProvider } from 'emotion-theming';
 import * as React from 'react';
 
 import { Typography } from './Typography';
@@ -6,39 +7,54 @@ import { Typography } from './Typography';
 /**
  * @param {{
  *   label: string,
- *   icon: React.ReactNode,
+ *   secondary?: boolean,
+ *   icon?: React.ReactNode,
  *   onPress: () => void,
  * }} param0
  */
-export const Button = ({ title, icon, onPress }) => {
+export const Button = ({ label, secondary, icon, onPress }) => {
   return (
     <Container
       onPress={onPress}
       accessible
-      accessibilityLabel={title}
+      accessibilityLabel={label}
       accessibilityRole='button'
-      hasIcon={!!icon}>
-      <Typography use='button'>{title}</Typography>
-      {icon}
+      hasIcon={!!icon}
+      secondary={secondary}>
+      <ThemeProvider theme={invertTextColors}>
+        <Typography use='button' secondary={secondary}>
+          {label}
+        </Typography>
+        {icon ? icon : null}
+      </ThemeProvider>
     </Container>
   );
 };
 
-const themePrimary = ({ theme }) => theme.primary;
-const themeOnPrimary = ({ theme }) => theme.onPrimary;
+const invertTextColors = theme => {
+  return {
+    ...theme,
+    textPrimaryOnBackground: theme.onPrimary,
+    textSecondaryOnBackground: theme.primary,
+  };
+};
+
+const themePrimary = ({ theme, secondary }) =>
+  secondary ? 'transparent' : theme.primary;
 const getBorderColor = ({ theme, secondary }) =>
   secondary ? theme.primary : 'transparent';
 
 const getJustifyContent = ({ hasIcon }) =>
   hasIcon ? 'space-between' : 'center';
 
-const Container = styled.TouchableOpacity`
+const Container = styled.View`
   background-color: ${themePrimary};
-  color: ${themeOnPrimary};
   height: 72px;
-  border: 1px solid ${getBorderColor};
+  border: 2px solid ${getBorderColor};
   padding: 16px;
   border-radius: 8px;
   flex-direction: row;
   justify-content: ${getJustifyContent};
+  align-content: center;
+  align-items: center;
 `;
