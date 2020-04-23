e5d1907116b24371f7e612647e426e0bcae5ffbb
Tim Stirrat <tim.stirrat@gmail.com>
2020-04-23 08:32:05 -0700

    Don't store the token, update instructions

--- app/locales/pull.sh
+++ app/locales/pull.sh
@@ -1,3 +1,8 @@
+# Usage:
+#   - Create a READ ONLY a token at https://app.lokalise.com/profile
+#   - Run the command from the root of the project with:
+#     LOKALISE_TOKEN=<token> yarn i18n:pull
+
 #!/bin/bash
 set -e
 
@@ -20,27 +25,27 @@ if ! found_exe lokalise2; then
   fi
 fi
 
-# echo "Downloading iOS *.strings"
-# lokalise2 file download \
-#   --add-newline-eof \
-#   --export-empty-as skip \
-#   --format strings \
-#   --include-description \
-#   --original-filenames \
-#   --unzip-to=ios \
-#   --export-sort=a_z \
-#   --config .lokalise.yml
+echo "Downloading iOS *.strings"
+lokalise2 file download \
+  --add-newline-eof \
+  --export-empty-as skip \
+  --format strings \
+  --include-description \
+  --original-filenames \
+  --unzip-to=ios \
+  --export-sort=a_z \
+  --config .lokalise.yml --token=$LOKALISE_TOKEN
 
-# echo "Downloading Android strings.xml"
-# lokalise2 file download \
-#   --add-newline-eof \
-#   --export-empty-as skip \
-#   --format xml \
-#   --include-description \
-#   --original-filenames \
-#   --unzip-to=android/app/src/main/res \
-#   --export-sort=a_z \
-#   --config .lokalise.yml
+echo "Downloading Android strings.xml"
+lokalise2 file download \
+  --add-newline-eof \
+  --export-empty-as skip \
+  --format xml \
+  --include-description \
+  --original-filenames \
+  --unzip-to=android/app/src/main/res \
+  --export-sort=a_z \
+  --config .lokalise.yml --token=$LOKALISE_TOKEN
 
 echo "Downloading i18next *.json files"
 lokalise2 file download \
@@ -55,4 +60,4 @@ lokalise2 file download \
   --unzip-to=app \
   --indentation=2sp \
   --json-unescaped-slashes \
-  --config .lokalise.yml
+  --config .lokalise.yml --token=$LOKALISE_TOKEN
