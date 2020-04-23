013ff5316bc27a97cc99979a688d7040750e332c
Fatkhi <dmitryfatkhi@gmail.com>
2020-04-23 16:02:27 -0700

    Add version check service

---
+++ app/constants/urls.js
@@ -0,0 +1,6 @@
+// need to get actual links to stores
+export const APPSTORE_URL = 'itms-apps://itunes.apple.com/us/app/id1501903733';
+export const PLAYSTORE_URL = 'market://details?id=edu.mit.privatekit';
+// placeholder json for testing, to be changed
+export const VERSION_URL =
+  'https://raw.githubusercontent.com/Fatkhi/covid-safe-paths/releases/versions.json';
