e7889ffbfd9143e5764581bfce654d019c4c56a5
Fatkhi <dmitryfatkhi@gmail.com>
2020-04-23 20:36:35 -0700

    Update link to yaml file

--- app/constants/versionCheck.js
+++ app/constants/versionCheck.js
@@ -1,6 +1,6 @@
 export const APPSTORE_URL = 'itms-apps://itunes.apple.com/us/app/id1508266966';
 export const PLAYSTORE_URL = 'market://details?id=org.pathcheck.covidsafepaths';
 export const VERSION_URL =
-  'https://raw.githack.com/Fatkhi/covid-safe-paths/versions-yaml/versions.yaml';
+  'https://rawcdn.githack.com/tripleblindmarket/covid-safe-paths/0008e269c355e14bb216bb6e69cf4b89e25012b2/versions.yaml';
 export const YAML_MANDATORY_VERSION_KEY = 'mandatory update for less than';
 export const YAML_LATEST_VERSION_KEY = 'latest version';
