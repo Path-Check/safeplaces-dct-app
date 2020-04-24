fbce7af13b490662ad946ba07275e55f6769d3fc
Tim Stirrat <tim.stirrat@gmail.com>
2020-04-23 19:14:05 -0700

    Use theme for checkbox

--- app/components/Checkbox.js
+++ app/components/Checkbox.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { Image, TouchableOpacity } from 'react-native';
 
 import boxCheckedIcon from './../assets/images/boxCheckedIcon.png';
 import boxUncheckedIcon from './../assets/images/boxUncheckedIcon.png';
-import Colors from '../constants/colors';
 import { Typography } from './Typography';
 
 export const Checkbox = props => {
@@ -18,16 +17,7 @@ export const Checkbox = props => {
         source={props.boxChecked === true ? boxCheckedIcon : boxUncheckedIcon}
         style={{ width: 25, height: 25, marginRight: 10 }}
       />
-      <Typography style={styles.checkboxText}>
-        {t('onboarding.eula_checkbox')}
-      </Typography>
+      <Typography use='body1'>{t('onboarding.eula_checkbox')}</Typography>
     </TouchableOpacity>
   );
 };
-
-const styles = StyleSheet.create({
-  checkboxText: {
-    color: Colors.WHITE,
-    fontSize: 18,
-  },
-});
