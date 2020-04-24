8d9c239b452a1c786707789fd86fd22dc3a67811
Ken Pugsley <>
2020-04-24 00:57:11 -0500

    change to rawcdn for the versions.yaml file

--- app/constants/versionCheck.js
+++ app/constants/versionCheck.js
@@ -1,6 +1,6 @@
 export const APPSTORE_URL = 'itms-apps://itunes.apple.com/us/app/id1508266966';
 export const PLAYSTORE_URL = 'market://details?id=org.pathcheck.covidsafepaths';
 export const VERSION_URL =
-  'https://raw.githack.com/tripleblindmarket/covid-safe-paths/develop/versions.yaml';
+  'https://rawcdn.githack.com/tripleblindmarket/covid-safe-paths/master/versions.yaml';
 export const YAML_MANDATORY_VERSION_KEY = 'mandatory upgrade version';
 export const YAML_LATEST_VERSION_KEY = 'upgrade version';
