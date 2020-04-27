06a86d450b20eb783cad096e99812a54bbea600e
Tim Stirrat <tim.stirrat@gmail.com>
2020-04-27 16:53:58 -0700

    Update language.js

--- e2e/helpers/language.js
+++ e2e/helpers/language.js
@@ -3,16 +3,15 @@ import merge from 'lodash/merge';
 import * as english from '../../app/locales/en.json';
 import * as haitian from '../../app/locales/ht.json';
 
-/**
- * Language strings for each locale. Strings are merged with english as a base
- * to emulate i18next's english fallback in the case of missing locale strings.
- */
 const languageStrings = {
   'en-US': english,
-  'ht-HT': merge({}, english, haitian),
+  'ht-HT': haitian,
 };
 
-export const languages = Object.keys(languageStrings).map(locale => [
-  locale,
-  languageStrings[locale],
-]);
+export const languages = Object.entries(languageStrings).map(
+  ([locale, strings]) => [
+    locale,
+    // fall back to english if not found in the locale strings
+    merge({}, english, strings),
+  ],
+);
