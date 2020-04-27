c9dfc6c3dcfd37e120b100ba586ed4b2db3c90e0
Tim Stirrat <tim.stirrat@gmail.com>
2020-04-27 16:13:49 -0700

    Fallback to english in e2e strings

--- e2e/helpers/language.js
+++ e2e/helpers/language.js
@@ -1,9 +1,15 @@
+import assign from 'lodash/assign';
+
 import * as english from '../../app/locales/en.json';
 import * as haitian from '../../app/locales/ht.json';
 
+/**
+ * Language strings for each locale. Strings are merged with english as a base
+ * to emulate i18next's english fallback in the case of missing locale strings.
+ */
 const languageStrings = {
   'en-US': english,
-  'ht-HT': haitian,
+  'ht-HT': assign({}, english, haitian),
 };
 
 export const languages = Object.keys(languageStrings).map(locale => [
