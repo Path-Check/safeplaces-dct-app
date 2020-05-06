89dc78cb57829b0f13a048de1ca5cb4fb902b0cc
Adam Putinski <adam.putinski@salesforce.com>
2020-05-06 14:35:49 -0400

    Remove WayPoints

--- app/constants/apis.js
+++ app/constants/apis.js
@@ -1,6 +1,3 @@
-export const WAYPOINTS_API =
-  'https://anypoint.mulesoft.com/mocking/api/v1/links/80889155-bd0e-4c2c-83b2-a68a47a361f5/waypoints';
-
 export const CATCHA_URL =
   'https://fathomless-mountain-29102.herokuapp.com/v1.0/captcha';
 
