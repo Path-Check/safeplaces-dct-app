78aaf3560536394860b0a30ec5bd64d5b3381f45
Tyler Yasaka <contact@tyleryasaka.me>
2020-05-06 20:43:22 -0700

    react native linking

--- android/app/src/main/java/org/pathcheck/covidsafepaths/MainApplication.java
+++ android/app/src/main/java/org/pathcheck/covidsafepaths/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.rnpermissions.RNPermissionsPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.facebook.react.ReactNativeHost;
