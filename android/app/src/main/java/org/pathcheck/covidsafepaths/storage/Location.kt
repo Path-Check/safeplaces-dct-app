8b60105fc040fe53892881235a610befec403b76
adam <adam@wearedragonfly.co>
2020-05-10 09:13:09 +0100

    Reimplemented github #759 post secure storage changes

--- android/app/src/main/java/org/pathcheck/covidsafepaths/storage/Location.kt
+++ android/app/src/main/java/org/pathcheck/covidsafepaths/storage/Location.kt
@@ -21,7 +21,6 @@ open class Location(
   @PrimaryKey var time: Long = 0,
   var latitude: Double = 0.0,
   var longitude: Double = 0.0,
-  var altitude: Double? = null,
   var speed: Float? = null,
   var accuracy: Float? = null,
   var bearing: Float? = null,
@@ -53,8 +52,7 @@ open class Location(
       return Location(
           time = backgroundLocation.time,
           latitude = backgroundLocation.latitude,
-          longitude = backgroundLocation.longitude,
-          altitude = if (backgroundLocation.hasAltitude()) backgroundLocation.altitude else null,
+          longitude = backgroundLocation.longitude,        
           speed = if (backgroundLocation.hasSpeed()) backgroundLocation.speed else null,
           accuracy = if (backgroundLocation.hasAccuracy()) backgroundLocation.accuracy else null,
           bearing = if (backgroundLocation.hasBearing()) backgroundLocation.bearing else null,
