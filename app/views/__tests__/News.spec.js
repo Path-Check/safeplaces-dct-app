703777673da295e3a3e402252e245983344d7155
Tim Stirrat <tim.stirrat@gmail.com>
2020-05-12 11:04:52 -0700

    Fix News tests

--- app/views/__tests__/News.spec.js
+++ app/views/__tests__/News.spec.js
@@ -1,10 +1,24 @@
 import 'react-native';
+
+import { render, wait } from '@testing-library/react-native';
 import React from 'react';
-import {render, wait} from '@testing-library/react-native';
+
+import { GetStoreData } from '../../helpers/General';
 import News from '../News';
 
+jest.mock('../../helpers/General', () => {
+  return {
+    GetStoreData: jest.fn(),
+  };
+});
+
+beforeEach(() => {
+  GetStoreData.mockResolvedValue('[]');
+});
+
 it('renders correctly', async () => {
-  const {asJSON} = render(<News />);
+  const { asJSON } = render(<News />);
+
   await wait();
 
   expect(asJSON()).toMatchSnapshot();
