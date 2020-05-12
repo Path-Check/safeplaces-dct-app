17926b352775ba25e55066a6314d7f4371e9605d
Tyler Roach <63416413+troach-sf@users.noreply.github.com>
2020-05-12 16:40:30 -0400

    fix(Location tracking) [iOS] Add location backfill for missing points (#798)

--- ios/COVIDSafePaths/storage/RealmSecureStorage.swift
+++ ios/COVIDSafePaths/storage/RealmSecureStorage.swift
@@ -11,8 +11,10 @@ import RealmSwift
 
 class RealmSecureStorage {
   
-  private static let MINIMUM_TIME_INTERVAL = 60 * 4
   private static let DAYS_TO_KEEP = 14
+  static let LOCATION_INTERVAL = 60 * 5
+  private static let MINIMUM_TIME_INTERVAL = 60 * 4
+  private static let MAX_BACKFILL_TIME = 60 * 60 * 24
   
   let keyLastSavedTime: String
   private let inMemory: Bool
@@ -45,6 +47,19 @@ class RealmSecureStorage {
     
     guard let realmConfig = getRealmConfig() else { return }
     let realm = try! Realm(configuration: realmConfig)
+    
+    let realmResults = realm.objects(Location.self)
+      .filter("\(Location.KEY_SOURCE)=\(Location.SOURCE_DEVICE)")
+      .sorted(byKeyPath: Location.KEY_TIME, ascending: false)
+    let previousLocation = realmResults.first
+    let previousTime = previousLocation?.time ?? 0
+    if currentTime - previousTime > RealmSecureStorage.MINIMUM_TIME_INTERVAL {
+      let assumedLocations = createAssumedLocations(previousLocation: previousLocation, newLocation: backgroundLocation)
+      try! realm.write {
+        realm.add(assumedLocations, update: .modified)
+      }
+    }
+    
     let location = Location.fromBackgroundLocation(backgroundLocation: backgroundLocation)
     try! realm.write {
       realm.add(location, update: .modified)
@@ -89,6 +104,29 @@ class RealmSecureStorage {
     try! realm.write {
       realm.delete(realmResults)
     }
+  }
+  
+  func createAssumedLocations(previousLocation: Location?, newLocation: MAURLocation) -> [Location] {
+    var assumedLocationsToInsert: [Location] = []
+    guard let previousLocation = previousLocation else {
+      return assumedLocationsToInsert
+    }
+    
+    let newLocationTime = Int(newLocation.time.timeIntervalSince1970)
+    let isNearbyPrevious = Location.areLocationsNearby(
+      lat1: previousLocation.latitude,
+      lon1: previousLocation.longitude,
+      lat2: newLocation.latitude.doubleValue,
+      lon2: newLocation.longitude.doubleValue)
+    let isTimeWithinThreshold = newLocationTime - previousLocation.time <= RealmSecureStorage.MAX_BACKFILL_TIME
+    if isNearbyPrevious && isTimeWithinThreshold {
+      let latestDesiredBackfill = newLocationTime - RealmSecureStorage.LOCATION_INTERVAL
+      let earliestDesiredBackfill = max(newLocationTime - RealmSecureStorage.MAX_BACKFILL_TIME, previousLocation.time + RealmSecureStorage.LOCATION_INTERVAL)
+      for time in stride(from: latestDesiredBackfill, through: earliestDesiredBackfill, by: -RealmSecureStorage.LOCATION_INTERVAL) {
+        assumedLocationsToInsert.append(Location.createAssumedLocation(time: time, latitude: previousLocation.latitude, longitude: previousLocation.longitude))
+      }
+    }
+    return assumedLocationsToInsert
   }
   
   func getCutoffTime() -> Int {
