29ce4ae15add2f081e9e603a30e85c56608dad75
Liz Falkner <elizabethfalkner09@gmail.com>
2020-05-13 19:32:57 -0400

    add in Platform reference

--- app/services/LocationService.js
+++ app/services/LocationService.js
@@ -1,5 +1,6 @@
 import BackgroundGeolocation from '@mauron85/react-native-background-geolocation';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import { Platform } from 'react-native';
 import PushNotification from 'react-native-push-notification';
 
 import { LOCATION_DATA, PARTICIPATE } from '../constants/storage';
@@ -224,7 +225,7 @@ export default class LocationServices {
         // required on iOS only (see fetchCompletionHandler docs: https://github.com/react-native-community/react-native-push-notification-ios)
         notification.finish(PushNotificationIOS.FetchResult.NoData);
       },
-      requestPermissions: true,
+      requestPermissions: Platform.OS === 'ios',
     });
 
     // PushNotificationIOS.requestPermissions();
