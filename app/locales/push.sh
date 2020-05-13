a8431dfb4816b529af830449698d6efae7b64a05
Tim Stirrat <tim.stirrat@gmail.com>
2020-05-13 14:58:38 -0700

    fix(Build) fix lokalise push script errors (#820)

--- app/locales/push.sh
+++ app/locales/push.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
+set -e
+
 # Usage:
 #   - Create a READ/WRITE token at https://app.lokalise.com/profile
 #   - Run the command from the root of the project with:
 #     LOKALISE_TOKEN=<token> yarn i18n:push
-
-#!/bin/bash
-set -e
 
 function found_exe() {
   hash "$1" 2>/dev/null
