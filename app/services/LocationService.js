6781b93aafb9f8f9c36d152aa64aa741108f67cb
efalkner <elizabethfalkner09@gmail.com>
2020-05-14 17:41:38 -0400

    fix(Location tracking) [Android] Prevent a FirebaseApp error (#826)

--- app/services/LocationService.js
+++ app/services/LocationService.js
@@ -1,6 +1,6 @@
 import BackgroundGeolocation from '@mauron85/react-native-background-geolocation';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 import PushNotification from 'react-native-push-notification';
 
 import { CROSSED_PATHS, PARTICIPATE } from '../constants/storage';
@@ -121,7 +121,9 @@ export default class LocationServices {
         // required on iOS only (see fetchCompletionHandler docs: https://github.com/react-native-community/react-native-push-notification-ios)
         notification.finish(PushNotificationIOS.FetchResult.NoData);
       },
-      requestPermissions: true,
+      // Setting the permissions to true causes a crash on Android, because that configuration requires Firebase
+      // https://github.com/zo0r/react-native-push-notification#usage
+      requestPermissions: Platform.OS === 'ios',
     });
 
     const locationData = new LocationData();
