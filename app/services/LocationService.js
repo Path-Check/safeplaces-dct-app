f76c18f4bcd6bd182cf972ea2d279ca15f439b64
Tim Stirrat <tim.stirrat@gmail.com>
2020-05-14 14:33:14 -0700

    Update LocationService.js

--- app/services/LocationService.js
+++ app/services/LocationService.js
@@ -121,6 +121,7 @@ export default class LocationServices {
         // required on iOS only (see fetchCompletionHandler docs: https://github.com/react-native-community/react-native-push-notification-ios)
         notification.finish(PushNotificationIOS.FetchResult.NoData);
       },
+      // Setting the permissions to true causes a crash on Android, because that configuration requires Firebase
       requestPermissions: Platform.OS === 'ios',
     });
 
