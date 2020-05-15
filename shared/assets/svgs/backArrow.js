d559bdfc167531a3bc22c2ed053b9cf1918b0b60
Matt Buckley <matt@nicethings.io>
2020-05-15 18:03:18 -0400

    Add assets to version control.

---
+++ shared/assets/svgs/backArrow.js
@@ -0,0 +1,5 @@
+export default `
+  <svg width="23" height="24" viewBox="0 0 23 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M11.4854 3L2.22803 12.2573L11.4854 21.5146" stroke="currentColor" stroke-opacity="0.4" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+`;
