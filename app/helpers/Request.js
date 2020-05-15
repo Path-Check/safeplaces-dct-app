9c5a8ac12ab30d4f921fa913552a071c640afe09
Yunior <yunior0509av@gmail.com>
2020-05-15 17:21:05 -0400

    fix location

--- app/helpers/Request.js
+++ app/helpers/Request.js
@@ -7,12 +7,17 @@ export function requestCovid19Hospitals() {
     'https://services5.arcgis.com/vzuQ2GBv7eL3S9SR/arcgis/rest/services/capa_HOSPITALES_del_SNS_para_COVID_para_apps/FeatureServer/1//query?where=OBJECTID%3E0&outFields=OBJECTID%2CNombre_del_Centro%2CNivel_de_atenci%C3%B3n%2CTelCentro%2Cx%2Cy&f=pjson&token=';
   return fetch(URL).then(({ data }) =>
     data.features
-      .map(({ attributes, geometry }) => {
+      .map(({ attributes }) => {
         if (!attributes) return null;
 
         // eslint-disable-next-line
-        const { OBJECTID, Nombre_del_Centro, TelCentro } = attributes;
-        const { x, y } = geometry;
+        const {
+          OBJECTID,
+          Nombre_del_Centro,
+          TelCentro,
+          FIELD_EXP_0: x,
+          FIELD_EXP_1: y,
+        } = attributes;
 
         if (!x || !y) return null;
 
@@ -30,23 +35,28 @@ export function requestCovid19Hospitals() {
 
 export function requestCovid19Laboratories() {
   const URL =
-    'https://services5.arcgis.com/vzuQ2GBv7eL3S9SR/arcgis/rest/services/Laboratorios_Clinicos_Hijo/FeatureServer/0//query?where=FID%3E0&outFields=FID%2CNombre%2CTelefono&f=pjson&token=';
+    'https://services5.arcgis.com/vzuQ2GBv7eL3S9SR/arcgis/rest/services/Laboratorios_Clinicos_Hijo/FeatureServer/0//query?where=FID>0&outFields=ID%2CNombre%2CTelefono%2Ccoord_X%2Ccoord_Y&f=pjson&token=';
   return fetch(URL).then(({ data }) =>
     data.features
-      .map(({ attributes, geometry }) => {
+      .map(({ attributes }) => {
         if (!attributes) return null;
 
         // eslint-disable-next-line
-        let { FID, Nombre, Telefono } = attributes;
-        const { x, y } = geometry;
-        if (!x || !y) return null;
+        let {
+          FID,
+          Nombre,
+          Telefono,
+          FIELD_EXP_1: Coord_X,
+          FIELD_EXP_2: Coord_Y,
+        } = attributes;
+        if (!Coord_X || !Coord_Y) return null;
 
         Telefono = Telefono === ' ' ? '(000) 000-0000' : Telefono;
 
         const utm = new utmObj('International');
 
         // This is to change coordinates from utm to degree
-        const { lat, lng } = utm.convertUtmToLatLng(x, y, 19, 'Q');
+        const { lat, lng } = utm.convertUtmToLatLng(Coord_X, Coord_Y, 19, 'Q');
 
         return {
           id: FID,
