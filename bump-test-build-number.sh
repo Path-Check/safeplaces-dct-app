58c6ba4a29819cd265d369ddae36c289b0987766
Tim Stirrat <tim.stirrat@gmail.com>
2020-05-20 15:14:11 -0700

    work locally with macosx and ubuntu

--- bump-test-build-number.sh
+++ bump-test-build-number.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 set -e # exit immediately on errors
 
-sudo apt-get install jq moreutils
+function found_bin() {
+  hash "$1" 2>/dev/null
+}
+
+function require_bin() {
+  if ! found_bin $1; then
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+
+      if ! found_bin brew; then
+        echo "You must install homebrew: https://docs.brew.sh/Installation"
+        exit 1
+      fi
+      brew install $2
+
+    else
+      sudo apt-get install $2
+    fi
+  fi
+}
+
+require_bin jq jq
+require_bin sponge moreutils
 
 # writes a timestamp to the end of the current package.json version
 # e.g. 1.0.65 -> 1.0.65.1589941056.235517
