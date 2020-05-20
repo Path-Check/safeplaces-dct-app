f3a9f9ced587bc2b281ad1d4d11cb280a38fbab1
Tim Stirrat <tim.stirrat@gmail.com>
2020-05-19 19:37:30 -0700

    moreutils

--- bump-test-build-number.sh
+++ bump-test-build-number.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # exit immediately on errors
 
-sudo apt-get install jq sponge
+sudo apt-get install jq moreutils
 
 # writes a timestamp to the end of the current package.json version
 # e.g. 1.0.65 -> 1.0.65.1589941056.235517
