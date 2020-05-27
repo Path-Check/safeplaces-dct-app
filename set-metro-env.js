7128fb8994197d00036438c4eb4456db8bc659b2
Matt Buckley <matt@nicethings.io>
2020-05-26 21:29:52 -0400

    Back out unnecessary changes.

--- set-metro-env.js
+++
@@ -1,8 +0,0 @@
-#!/bin/node
-const fs = require("fs");
-
-const environment = process.argv[2]
-
-const envFileContent = require(`./metro.config.${environment}.json`);
-
-fs.writeFileSync("metro-env.json", JSON.stringify(envFileContent, undefined, 2));
