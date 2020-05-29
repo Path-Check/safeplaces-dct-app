8b2cda201641f352f74960b8645a4f4009505cb8
Tim Stirrat <tim.stirrat@gmail.com>
2020-05-28 18:02:21 -0700

    ugh

--- append-sha-to-version.sh
+++ append-sha-to-version.sh
@@ -26,16 +26,13 @@ require_bin sponge moreutils
 
 # writes a timestamp to the end of the current package.json version
 # e.g. 1.0.65 -> 1.0.65.1589941056.235517
-cat package.json | grep version
 
 # PRs create a merge commit into develop, so we cannot use $GITHUB_SHA
 # instead we need ${{ github.head_ref }} which is the pure head ref
-echo "GITHUB_REF: $GITHUB_REF"
-echo "GITHUB_REF: $GITHUB_REF"
-echo "HEAD_REF_SHA: $HEAD_REF_SHA"
-SHA=$(git rev-parse --short $GITHUB_REF)
-echo "SHA: $SHA"
+echo "HEAD_REF: $HEAD_REF"
+git fetch
+git checkout $HEAD_REF
+SHA=$(git rev-parse --short $HEAD_REF)
+echo "using SHA: $SHA"
 
 jq --arg SHA "$SHA" '.version=(.version) + "-" + $SHA' package.json | sponge package.json
-
-cat package.json | grep version
