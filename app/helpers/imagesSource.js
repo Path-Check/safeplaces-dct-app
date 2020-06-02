91b001ea215b9f365fc8e6471ffacc5613e3c0f4
David De La Rosa <davidrr261096@hotmail.com>
2020-06-02 12:20:57 -0400

    fixing news header (#45)
    * fixing news header

--- app/helpers/imagesSource.js
+++
@@ -1,8 +0,0 @@
-export default function(dataList) {
-  const data = dataList.news.map(item => ({
-    ...item,
-    img: { source: { uri: Object.values(item.img)[1] } },
-  }));
-
-  return data;
-}
