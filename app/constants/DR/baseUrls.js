5dc30beb7088f7007c96ab4d655351e4ad685a63
Adnan <adnanitt@gmail.com>
2020-06-04 16:44:23 -0400

    Added secrets for URL

--- app/constants/DR/baseUrls.js
+++ app/constants/DR/baseUrls.js
@@ -1,7 +1,7 @@
-export const MEPYD_C5I_SERVICE = __DEV__
-  ? 'https://webapps.mepyd.gob.do'
-  : 'https://appscovid.mide.gob.do';
-export const COV_CASES_SERVICE = 'https://corona.lmao.ninja/v2/countries/do';
-export const FIREBASE_SERVICE = 'https://covid-dr.appspot.com';
-export const REST_COUNTRIES_SERVICE = 'https://restcountries.eu/rest/v2/all';
-export const HEALTH_SERVICES_SERVICE = 'https://services5.arcgis.com';
+import env from 'react-native-config';
+
+export const MEPYD_C5I_SERVICE = env.MEPYD_C5I_URL;
+export const COV_CASES_SERVICE = env.COV_CASES_URL;
+export const FIREBASE_SERVICE = env.FIREBASE_URL;
+export const REST_COUNTRIES_SERVICE = env.REST_COUNTRIES_URL;
+export const HEALTH_SERVICES_SERVICE = env.HEALTH_SERVICES_URL;
