eea81c438600b6b0e7a7ed09f5f2d0734f3bdff5
Matt Buckley <matt@nicethings.io>
2020-06-06 09:29:21 -0400

    Create EN debug menu UI.

---
+++ app/views/Settings/ENDev.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { BackHandler, ScrollView } from 'react-native';
+
+import { NavigationBarWrapper } from '../../components';
+import { Item } from './Item';
+import { Section } from './Section';
+
+export const EN_DEBUG_MENU_SCREEN_NAME = 'ENDev';
+
+export const ENDev = ({ navigation }) => {
+  useEffect(() => {
+    const handleBackPress = () => {
+      navigation.goBack();
+      return true;
+    };
+
+    BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
+    };
+  }, [navigation]);
+
+  const backToSettings = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <NavigationBarWrapper title={'EN Debug Menu'} onBackPress={backToSettings}>
+      <ScrollView>
+        <Section>
+          <Item label='Detect Exposures Now' />
+          <Item label='Simulate Exposure Detection Error' />
+          <Item label='Simulate Exposure' />
+          <Item label='Simulate Positive Diagnosis' />
+          <Item label='Disable Exposure Notifications' />
+          <Item label='Reset Exposure Detection Error' />
+          <Item label='Reset Exposures' />
+          <Item label='Reset Test Results' last />
+        </Section>
+        <Section last>
+          <Item label='Get and Post Diagnosis Keys' />
+          <Item label='Reset Diagnosis Keys' last />
+        </Section>
+      </ScrollView>
+    </NavigationBarWrapper>
+  );
+};
