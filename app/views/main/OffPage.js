02a0fc4716d6d5f37830b91fd57c68fb3f6220f5
Thomas Paresi <thomas.paresi@thoughtbot.com>
2020-06-10 12:59:33 -0500

    Create main screens for both states for Exposure notifications

--- app/views/main/OffPage.js
+++ app/views/main/OffPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import {
   Dimensions,
   ImageBackground,
@@ -17,8 +16,7 @@ import { useAssets } from '../../TracingStrategyAssets';
 import { styles } from './style';
 
 export const OffPage = () => {
-  const { t } = useTranslation();
-  const { offPageCta, offPageButton } = useAssets();
+  const { offPageHeader, offPageCta, offPageButton } = useAssets();
   const size = Dimensions.get('window').height;
 
   return (
@@ -46,7 +44,7 @@ export const OffPage = () => {
           </View>
           <View style={styles.contentBelowPulse}>
             <Text style={styles.mainTextBelow}>
-              {t('label.home_setting_off_header')}
+              {offPageHeader}
             </Text>
             <Typography style={styles.subheaderText}>{offPageCta}</Typography>
             <Button
