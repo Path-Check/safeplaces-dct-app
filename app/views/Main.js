17b93dc23973382cca9d0c70684813f67cae5fa5
Thomas Paresi <thomas.paresi@thoughtbot.com>
2020-06-18 12:07:25 -0500

    Use Main as default naming for GPS
    Also, remove some logic in BTMain that will be replaced by the event emitter logic

--- app/views/Main.js
+++ app/views/Main.js
@@ -27,46 +27,17 @@ export const BTMain = () => {
     StatusBar.setTranslucent(true);
   }
 
-  const navigation = useNavigation();
   const { authorizationStatus, requestENAuthorization } = useContext(ExposureNotificationsContext);
   console.log("Auth status: ", authorizationStatus);
-  const [trackingInfo, setTrackingInfo] = useState({ canTrack: authorizationStatus === 'authorized' });
 
-  const updateStateInfo = useCallback(async () => {
-    setTrackingInfo({ canTrack: authorizationStatus === 'authorized' });
-  }, [authorizationStatus, setTrackingInfo]);
-
-  useEffect(() => {
-    updateStateInfo();
-    // refresh state if user backgrounds app
-    AppState.addEventListener('change', updateStateInfo);
-
-    // refresh state if settings change
-    const unsubscribe = navigation.addListener('focus', updateStateInfo);
-
-    // handle back press
-    BackHandler.addEventListener('hardwareBackPress', handleBackPress);
-
-    return () => {
-      AppState.removeEventListener('change', updateStateInfo);
-      BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
-      unsubscribe();
-    };
-  }, [navigation, updateStateInfo]);
-
-  const handleBackPress = () => {
-    BackHandler.exitApp(); // works best when the goBack is async
-    return true;
-  };
-
-  if (!trackingInfo.canTrack) {
-    return <TracingOffScreen onPress={requestENAuthorization} />;
-  } else {
+  if (authorizationStatus === 'ENABLED') {
     return <AllServicesOnScreen />;
+  } else {
+    return <TracingOffScreen onPress={requestENAuthorization} />;
   }
 };
 
-export const GPSMain = () => {
+export const Main = () => {
   if (isPlatformAndroid()) {
     StatusBar.setBackgroundColor(Colors.transparent);
     StatusBar.setBarStyle('light-content');
