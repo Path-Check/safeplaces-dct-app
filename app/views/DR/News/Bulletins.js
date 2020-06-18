448e4534b85abff2de36df794defbea617e26153
David De La Rosa <davidrr261096@hotmail.com>
2020-06-18 11:12:27 -0400

    Feat: pagination bulletin screen (#95)
    * adding pagination to bulletins screen

--- app/views/DR/News/Bulletins.js
+++ app/views/DR/News/Bulletins.js
@@ -1,17 +1,28 @@
+import { Button, Text } from 'native-base';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { BackHandler, ScrollView, StyleSheet, View } from 'react-native';
+import {
+  ActivityIndicator,
+  BackHandler,
+  ScrollView,
+  StyleSheet,
+  View,
+} from 'react-native';
 
 import iconImgBulletin from '../../../assets/images/bulletin.jpg';
 import imgBulletins from '../../../assets/images/bulletins.jpg';
 import HeaderImage from '../../../components/DR/ActionCards/HeaderImage';
 import List from '../../../components/DR/ActionCards/List';
 import { FIREBASE_SERVICE } from '../../../constants/DR/baseUrls';
+import buttonStyle from '../../../constants/DR/buttonStyles';
 import fetch from '../../../helpers/Fetch';
 
 const BULLETINS_URL = `${FIREBASE_SERVICE}/bulletins`;
 export default function BulletinsScreen({ navigation }) {
   const [bulletins, setBulletins] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isDisable, setIsDisable] = useState(false);
+  const [isNotLastPage, setIsNotLastPage] = useState(true);
   const { t } = useTranslation();
 
   const handleBackPress = () => {
@@ -19,14 +30,36 @@ export default function BulletinsScreen({ navigation }) {
     return true;
   };
 
+  const addImgToBulletin = bulletinList => {
+    return bulletinList.map(item => ({
+      ...item,
+      img: { source: iconImgBulletin },
+    }));
+  };
+
+  const onPress = () => {
+    const { order } = bulletins[bulletins.length - 1] || {};
+    if (!order || order <= 10) {
+      setIsNotLastPage(false);
+      setIsDisable(true);
+    }
+    setIsLoading(true);
+    fetch(`${BULLETINS_URL}?endAt=${order - 1}`)
+      .then(({ data }) => {
+        const bulletinsData = addImgToBulletin(data.bulletins);
+        setBulletins(bulletins.concat(bulletinsData));
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setBulletins([]);
+      });
+  };
+
   useEffect(() => {
     BackHandler.addEventListener('hardwareBackPress', handleBackPress);
     fetch(BULLETINS_URL)
       .then(({ data }) => {
-        const bulletinsData = data.bulletins.map(item => ({
-          ...item,
-          img: { source: iconImgBulletin },
-        }));
+        const bulletinsData = addImgToBulletin(data.bulletins);
         setBulletins(bulletinsData);
       })
       .catch(() => {
@@ -47,6 +80,24 @@ export default function BulletinsScreen({ navigation }) {
           navigation={navigation}
           switchScreenTo='PDFView'
         />
+        <View style={styles.containerPagination}>
+          {isNotLastPage && isLoading ? (
+            <ActivityIndicator size='large' />
+          ) : (
+            <Button
+              disabled={isDisable}
+              style={
+                isDisable
+                  ? { display: 'none' }
+                  : { ...buttonStyle.buttonStyle, marginLeft: 0 }
+              }
+              onPress={onPress}>
+              <Text style={buttonStyle.buttonText}>
+                {t('label.launch_next')}
+              </Text>
+            </Button>
+          )}
+        </View>
       </ScrollView>
     </View>
   );
@@ -55,5 +106,9 @@ export default function BulletinsScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  containerPagination: {
+    alignItems: 'center',
+    padding: 20,
   },
 });
