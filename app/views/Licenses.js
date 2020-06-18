5accc08d1530e0eef645556603e1cba43fb84385
Jose Francisco Estrella <51418416+JoseFcoEstrella@users.noreply.github.com>
2020-06-18 10:44:17 -0400

    Add spanish to privacy policy (#94)
    * Add spanish to privacy policy

--- app/views/Licenses.js
+++ app/views/Licenses.js
@@ -22,8 +22,13 @@ import { Theme } from '../constants/themes';
 const PRIVACY_POLICY_URL =
   'https://docs.google.com/document/d/17u0f8ni9S0D4w8RCUlMMqxAlXKJAd2oiYGP8NUwkINo/edit';
 
+const PRIVACY_POLICY_URL_ES =
+  'https://docs.google.com/document/d/1znFbxn02pqVFFR9EDQh7jc28qBjWWI-MH7G50EFxEgs/edit';
+
 export const LicensesScreen = ({ navigation }) => {
   const { t, i18n } = useTranslation();
+  const urlPrivacyPolicy =
+    i18n.language === 'es' ? PRIVACY_POLICY_URL_ES : PRIVACY_POLICY_URL;
 
   const backToMain = () => {
     navigation.goBack();
@@ -35,7 +40,7 @@ export const LicensesScreen = ({ navigation }) => {
   };
 
   const handleTermsOfUsePressed = () => {
-    Linking.openURL(PRIVACY_POLICY_URL);
+    Linking.openURL(urlPrivacyPolicy);
   };
 
   useEffect(() => {
@@ -89,7 +94,7 @@ export const LicensesScreen = ({ navigation }) => {
           style={styles.termsInfoRow}>
           <Typography
             use='headline2'
-            onPress={() => Linking.openURL(PRIVACY_POLICY_URL)}>
+            onPress={() => Linking.openURL(urlPrivacyPolicy)}>
             {t('label.privacy_policy')}
           </Typography>
           <View style={styles.arrowContainer}>
