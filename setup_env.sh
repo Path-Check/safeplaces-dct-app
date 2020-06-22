25b1fb1a5053e5e920840a9264ad13702e064959
Matt Buckley <matt@nicethings.io>
2020-06-22 17:48:57 -0400

    Leave GPS env files unchanged.

--- setup_env.sh
+++ setup_env.sh
@@ -5,6 +5,3 @@ cp -R pathcheck-mobile-resources/environment/ ./
 git update-index --assume-unchanged .env.bt
 git update-index --assume-unchanged .env.bt.staging
 git update-index --assume-unchanged .env.bt.release
-git update-index --assume-unchanged .env.gps
-git update-index --assume-unchanged .env.gps.staging
-git update-index --assume-unchanged .env.gps.release
