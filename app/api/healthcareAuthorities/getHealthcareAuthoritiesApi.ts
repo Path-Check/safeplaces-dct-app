71b887e47f3d6d501c7c3b36d4823f6d45d2c968
Jacob Jaffe <jacobmaxjaffe@gmail.com>
2020-06-23 12:14:11 -0400

    HA List Empty State (#1072)
    * empty state for ha list
    
    * interpret null as empty array from yaml

--- app/api/healthcareAuthorities/getHealthcareAuthoritiesApi.ts
+++ app/api/healthcareAuthorities/getHealthcareAuthoritiesApi.ts
@@ -25,7 +25,8 @@ const getHealthcareAuthoritiesApi = async (
   yamlUrl: string = AUTHORITIES_YAML_ROUTE,
 ): Promise<HealthcareAuthority[]> => {
   const yamlString = await fetch(yamlUrl).then((res) => res.text());
-  const { authorities } = Yaml.safeLoad(yamlString);
+  let { authorities } = Yaml.safeLoad(yamlString);
+  if (authorities === null) authorities = [];
   if (!Array.isArray(authorities)) {
     throw new Error('authorities yaml did not return an array of authorities');
   }
