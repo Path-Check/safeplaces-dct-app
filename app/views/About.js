fbdac2f7c8441e4c3a14971600540b4ec17c0747
Jacob <JacobMaxJaffe@gmail.com>
2020-06-24 15:54:30 -0400

    1.1.1b2

--- app/views/About.js
+++ app/views/About.js
@@ -17,7 +17,7 @@ import { useAssets } from '../TracingStrategyAssets';
 import { Colors, Spacing } from '../styles';
 
 export const AboutScreen = ({ navigation }) => {
-  const version = '1.1.0 (7)';
+  const version = '1.1.1 (2)';
   const { t } = useTranslation();
   const { aboutHeader } = useAssets();
 
