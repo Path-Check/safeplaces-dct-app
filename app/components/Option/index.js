22a9db5db7ba453fd74168fcee94b13122a8e50c
Eric Bailey <eric.w.bailey@gmail.com>
2020-06-26 11:39:49 -0400

    Tweak input border width and color

--- app/components/Option/index.js
+++ app/components/Option/index.js
@@ -28,14 +28,14 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
     borderColor: Colors.secondaryBorder,
     borderRadius: Spacing.xxSmall,
-    borderWidth: 1,
+    borderWidth: 2,
     marginBottom: Spacing.small,
     paddingHorizontal: Spacing.medium,
     paddingVertical: Spacing.medium,
   },
   containerSelected: {
     backgroundColor: Colors.secondaryBackground,
-    borderColor: Colors.primaryViolet,
+    borderColor: Colors.secondaryBlue,
   },
   primary: {
     alignItems: 'flex-start',
