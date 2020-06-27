499645bc0c6636ec883ae912d34c8f8f7889de31
Vlatko Ivetic <vlatko.ivetic@fer.hr>
2020-06-28 00:21:51 +0200

    fix logic in middleware

--- app/store/middleware/onChangedSelectedHealthAuthorities.ts
+++ app/store/middleware/onChangedSelectedHealthAuthorities.ts
@@ -5,11 +5,12 @@ import IntersectService from '../../services/IntersectService';
 const onChangedSelectedHealthAuthorities: Middleware<Dispatch> = () => (
   next: Dispatch<AnyAction>,
 ) => (action: AnyAction): unknown => {
+  const result = next(action);
+
   if (action.type === TOGGLE_SELECTED_HEALTHCARE_AUTHORITY) {
     IntersectService.checkIntersect(true);
   }
-
-  return next(action);
+  return result;
 };
 
 export default onChangedSelectedHealthAuthorities;
