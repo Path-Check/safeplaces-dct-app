bd226c3380194400a0c2cc62300c4b6ff7de3bec
Vlatko Ivetic <vlatko.ivetic@fer.hr>
2020-06-29 01:50:05 +0200

    move logic from BackgroundTaskService to IntersectService, pass exposure info data to component

--- app/helpers/Intersect.d.ts
+++ app/helpers/Intersect.d.ts
@@ -1,7 +1,8 @@
 import { checkIntersect as intersect } from './Intersect';
 import { HealthcareAuthority } from '../store/types';
+import { ExposureDatum } from '../exposureHistory';
 
 export const checkIntersect: (
   healthcareAuthorities: HealthcareAuthority[] | null,
   bypassTimer: boolean,
-) => Promise<void> = intersect;
+) => Promise<Record<number, ExposureDatum>> = intersect;
--- app/services/IntersectService.ts
+++ app/services/IntersectService.ts
@@ -1,6 +1,21 @@
 import { checkIntersect as intersect } from '../helpers/Intersect';
 import { HealthcareAuthority } from '../store/types';
-import BackgroundTaskService from './BackgroundTaskService';
+import { emitGPSExposureInfo } from '../gps/exposureInfo';
+import { INTERSECT_INTERVAL } from '../constants/history';
+import BackgroundFetch from 'react-native-background-fetch';
+
+const bgTaskConfig = {
+  minimumFetchInterval: INTERSECT_INTERVAL,
+  // Android options
+  forceAlarmManager: false, // <-- Set true to bypass JobScheduler.
+  stopOnTerminate: false,
+  startOnBoot: true,
+  requiredNetworkType: BackgroundFetch.NETWORK_TYPE_NONE, // Default
+  requiresCharging: false, // Default
+  requiresDeviceIdle: false, // Default
+  requiresBatteryNotLow: false, // Default
+  requiresStorageNotLow: false, // Default
+};
 
 class IntersectService {
   isServiceRunning = false;
@@ -16,18 +31,37 @@ class IntersectService {
     }
     this.isServiceRunning = true;
 
-    intersect(healthcareAuthorities, bypassTimer).then(() => {
+    intersect(healthcareAuthorities, bypassTimer).then((result) => {
       this.isServiceRunning = false;
+
       if (this.nextJob) {
         const job = this.nextJob;
         this.nextJob = null;
         this.checkIntersect(job, bypassTimer);
+      } else {
+        emitGPSExposureInfo(result);
       }
     });
 
-    BackgroundTaskService.start();
+    this.startBackgroundTask();
     return 'started';
   };
+
+  private startBackgroundTask = () => {
+    BackgroundFetch.configure(
+      bgTaskConfig,
+      async (taskId) => {
+        console.log('[js] Received background-fetch event: ', taskId);
+        this.checkIntersect(null, false);
+        BackgroundFetch.finish(taskId);
+      },
+      (error) => {
+        console.log('[js] RNBackgroundFetch failed to start', error);
+      },
+    );
+  };
+
+  // TODO: add mechanism for stopping background task
 }
 
 const singleton = new IntersectService();
