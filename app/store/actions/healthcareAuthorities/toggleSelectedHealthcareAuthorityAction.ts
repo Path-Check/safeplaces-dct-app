8faf901235a24e12163dafa8c361b773f1558309
Vlatko Ivetic <vlatko.ivetic@fer.hr>
2020-06-28 02:33:04 +0200

    remove export

--- app/store/actions/healthcareAuthorities/toggleSelectedHealthcareAuthorityAction.ts
+++ app/store/actions/healthcareAuthorities/toggleSelectedHealthcareAuthorityAction.ts
@@ -1,7 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
 import type { HealthcareAuthority } from '../../types';
 
-export const TOGGLE_SELECTED_HEALTHCARE_AUTHORITY =
+const TOGGLE_SELECTED_HEALTHCARE_AUTHORITY =
   'TOGGLE_SELECTED_HEALTHCARE_AUTHORITY';
 
 const toggleSelectedHealthcareAuthorityAction = createAction<{
