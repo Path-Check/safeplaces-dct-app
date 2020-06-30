cfd82a6298463a5c365dd98d6fa5c79f57e544f3
Vlatko Ivetic <vlatko.ivetic@fer.hr>
2020-06-30 20:03:35 +0200

    revert deletion of BackgroundTaskService

--- app/services/IntersectService.ts
+++ app/services/IntersectService.ts
@@ -1,28 +1,14 @@
 import { checkIntersect as intersect } from '../helpers/Intersect';
 import { HealthcareAuthority } from '../store/types';
 import { emitGPSExposureInfo } from '../gps/exposureInfo';
-import { INTERSECT_INTERVAL } from '../constants/history';
-import BackgroundFetch from 'react-native-background-fetch';
-
-const bgTaskConfig = {
-  minimumFetchInterval: INTERSECT_INTERVAL,
-  // Android options
-  forceAlarmManager: false, // <-- Set true to bypass JobScheduler.
-  stopOnTerminate: false,
-  startOnBoot: true,
-  requiredNetworkType: BackgroundFetch.NETWORK_TYPE_NONE, // Default
-  requiresCharging: false, // Default
-  requiresDeviceIdle: false, // Default
-  requiresBatteryNotLow: false, // Default
-  requiresStorageNotLow: false, // Default
-};
+import BackgroundTaskService from './BackgroundTaskService';
 
 class IntersectService {
   isServiceRunning = false;
   private nextJob: HealthcareAuthority[] | null = null;
 
   constructor() {
-    this.startBackgroundTask();
+    BackgroundTaskService.start();
   }
 
   checkIntersect = (
@@ -49,22 +35,6 @@ class IntersectService {
 
     return 'started';
   };
-
-  private startBackgroundTask = () => {
-    BackgroundFetch.configure(
-      bgTaskConfig,
-      async (taskId) => {
-        console.log('[js] Received background-fetch event: ', taskId);
-        this.checkIntersect(null, false);
-        BackgroundFetch.finish(taskId);
-      },
-      (error) => {
-        console.log('[js] RNBackgroundFetch failed to start', error);
-      },
-    );
-  };
-
-  // TODO: add mechanism for stopping background task
 }
 
 const singleton = new IntersectService();
