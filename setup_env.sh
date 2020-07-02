43730201f9df7d252229d200a74bb7c45e1fd71c
Matt Buckley <matt@nicethings.io>
2020-07-02 09:52:01 -0400

    Set up set-config script

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,30 @@
 #!/bin/bash                                                                                                         
 
-cp -R pathcheck-mobile-resources/environment/ ./
+DOWNLOAD_PATH=$1
+
+git submodule update --remote --merge
+
+if [[ $# -eq 0 ]] ; then
+    tput setaf 1; echo 'You must supply a config argument'
+    exit 1
+fi
+
+if [ ! -f pathcheck-mobile-resources/environment/$DOWNLOAD_PATH/.env.bt ]; then
+    tput setaf 1; echo 'pathcheck-mobile-resources/environment/'$DOWNLOAD_PATH'/.env.bt does not exist'
+    exit 1
+fi
+
+if [ ! -f pathcheck-mobile-resources/environment/$DOWNLOAD_PATH/.env.bt.staging ]; then
+    tput setaf 1; echo 'pathcheck-mobile-resources/environment/'$DOWNLOAD_PATH'/.env.bt.staging does not exist'
+    exit 1
+fi
+
+if [ ! -f pathcheck-mobile-resources/environment/$DOWNLOAD_PATH/.env.bt.release ]; then
+    tput setaf 1; echo 'pathcheck-mobile-resources/environment/'$DOWNLOAD_PATH'/.env.bt.release does not exist'
+    exit 1
+fi
+
+cp -r pathcheck-mobile-resources/environment/$DOWNLOAD_PATH/. ./
 
 git update-index --assume-unchanged .env.bt
 git update-index --assume-unchanged .env.bt.staging
