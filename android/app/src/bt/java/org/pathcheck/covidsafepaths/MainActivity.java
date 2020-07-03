bf647c00d52d3eba7b557eccea2cf559ae4cd01a
Dick Lucas <dick@fullsend.io>
2020-07-03 11:30:51 -0700

    Request permission right after it is accepted. This allows isEnabled to report the correct status next time on app start.

--- android/app/src/bt/java/org/pathcheck/covidsafepaths/MainActivity.java
+++ android/app/src/bt/java/org/pathcheck/covidsafepaths/MainActivity.java
@@ -18,10 +18,12 @@ import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.nearby.exposurenotification.ExposureNotificationStatusCodes;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import org.devio.rn.splashscreen.SplashScreen;
 
+import covidsafepaths.bt.exposurenotifications.ExposureNotificationClientWrapper;
 import covidsafepaths.bt.exposurenotifications.utils.CallbackMessages;
 import covidsafepaths.bt.exposurenotifications.utils.RequestCodes;
 import covidsafepaths.bt.exposurenotifications.utils.Util;
@@ -32,6 +34,37 @@ public class MainActivity extends ReactActivity {
   protected void onCreate(Bundle savedInstanceState) {
     SplashScreen.show(this, R.style.SplashTheme);
     super.onCreate(savedInstanceState);
+    hitIt(5);
+
+  }
+
+  private void hitIt(int repeat) {
+    if(repeat == 0) {
+      return;
+    }
+    ExposureNotificationClientWrapper.get(this)
+            .isEnabled().addOnSuccessListener(
+            enabled -> {
+              if(enabled) {
+                Log.d("HELLO123", " MainActivity enabled");
+                hitIt(repeat - 1);
+                //callback.invoke(Util.toWritableArray(CallbackMessages.EN_AUTHORIZATION_AUTHORIZED, CallbackMessages.EN_ENABLEMENT_ENABLED));
+              } else {
+                Log.d("HELLO123", "MainActivity disabled");
+                hitIt(repeat - 1);
+                //callback.invoke(Util.toWritableArray(CallbackMessages.EN_AUTHORIZATION_UNAUTHORIZED, CallbackMessages.EN_ENABLEMENT_DISABLED));
+              }
+            })
+            .addOnFailureListener(
+                    exception -> {
+                      Log.d("HELLO123", "failure");
+                      if (!(exception instanceof ApiException)) {
+                        //callback.invoke(CallbackMessages.ERROR_UNKNOWN);
+                      } else {
+                        ApiException apiException = (ApiException) exception;
+                        //callback.invoke(apiException.getStatus().toString());
+                      }
+                    });
   }
 
   /**
@@ -85,6 +118,34 @@ public class MainActivity extends ReactActivity {
     if (resultCode == Activity.RESULT_OK) {
       Log.d("MainActivity", "RESULT_OK");
       sendEvent(true);
+      final ReactContext reactContext = getReactNativeHost().getReactInstanceManager().getCurrentReactContext();
+      ExposureNotificationClientWrapper.get(reactContext)
+              .start()
+              .addOnSuccessListener(
+                      unused -> {
+                        Log.d("HELLO123", "request success");
+                      })
+              .addOnFailureListener(
+                      exception -> {
+                        Log.d("HELLO123", "request failure");
+                        if (!(exception instanceof ApiException)) {
+                          Log.d("HELLO123", "error unkown");
+                          return;
+                        }
+                        ApiException apiException = (ApiException) exception;
+                        if (apiException.getStatusCode()
+                                == ExposureNotificationStatusCodes.RESOLUTION_REQUIRED) {
+                          Log.d("HELLO123", "res required");
+                          MainActivity mainActivity = (MainActivity) reactContext.getCurrentActivity();
+                          mainActivity.showPermission(apiException);
+
+                        } else {
+                        }
+                      })
+              .addOnCanceledListener(() -> {
+                Log.d("HELLO123", "request cancelled");
+                //callback.invoke(CallbackMessages.CANCELLED);
+              });
     } else {
       Log.d("MainActivity", "NOT RESULT_OK");
       sendEvent(false);
