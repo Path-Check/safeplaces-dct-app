9a7c7f30ca8ce830526624e8d66bb7c31700703d
Tin Nguyen <tindn14@gmail.com>
2020-07-06 13:48:43 -0400

    install reactotron and react-native-netinfo

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ import App from './App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+if (__DEV__) {
+  import('./reactotron.config').then(() =>
+    console.log('Reactotron Configured'),
+  );
+}
