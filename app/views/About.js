fc8bb6f161a3273f3ad244bc6142c92d35f905fa
Jacob <JacobMaxJaffe@gmail.com>
2020-07-07 10:45:49 -0400

    undo about changess

--- app/views/About.js
+++ app/views/About.js
@@ -12,6 +12,7 @@ import {
 } from 'react-native';
 
 import { useStrategyContent } from '../TracingStrategyContext';
+import packageJson from '../../package.json';
 import { NavigationBarWrapper, Typography } from '../components';
 
 import { useDispatch } from 'react-redux';
@@ -19,8 +20,6 @@ import toggleAllowFeatureFlagsAction from '../store/actions/featureFlags/toggleA
 import { Colors, Spacing, Typography as TypographyStyles } from '../styles';
 
 const CLICKS_TO_ENABLE_FEATURE_FLAGS = 10;
-
-const version = '1.1.3 (2) ALPHA';
 
 export const AboutScreen = ({ navigation }) => {
   const dispatch = useDispatch();
@@ -70,7 +69,7 @@ export const AboutScreen = ({ navigation }) => {
             </Typography>
 
             <Typography style={styles.aboutSectionParaContent}>
-              {version}
+              {packageJson.version}
             </Typography>
           </View>
           <View style={styles.row}>
