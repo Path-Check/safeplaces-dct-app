a0adf879a61cb8e26ad5e580baee437450761337
Matt Buckley <matt.buckley@rocketinsights.com>
2020-07-12 21:21:57 -0400

    Accommodate key file processing limit on ios. (#1220)
    * Accommodate key file processing limit on ios
    
    * Remove unnecessary check.

--- ios/BT/API/Model/UserState.swift
+++ ios/BT/API/Model/UserState.swift
@@ -6,8 +6,10 @@ class UserState: Object {
   static let id = 0
 
   @objc dynamic var id: Int = UserState.id
-  @objc dynamic var dateLastPerformedExposureDetection: Date? = nil
+  @objc dynamic var dateLastPerformedFileCapacityReset: Date? = nil
+  @objc dynamic var remainingDailyFileProcessingCapacity: Int = 0
   @objc dynamic var exposureDetectionErrorLocalizedDescription: String = .default
+  @objc dynamic var urlOfMostRecentlyDetectedKeyFile: String = .default
   dynamic var exposures: List<Exposure> = List<Exposure>()
 
   override class func primaryKey() -> String? {
--- ios/BT/Extensions/Foundation/Date+Extensions.swift
+++ ios/BT/Extensions/Foundation/Date+Extensions.swift
@@ -2,4 +2,11 @@ extension Date {
   var posixRepresentation: Int {
     Int(timeIntervalSince1970) * 1000
   }
+
+  func difference(from date: Date, only component: Calendar.Component, calendar: Calendar = .current) -> Int {
+    let a = calendar.component(component, from: self)
+    let b = calendar.component(component, from: date)
+    return a - b
+  }
+
 }
