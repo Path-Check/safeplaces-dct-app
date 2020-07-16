0b848410b0377083df253c789ae52f4de5edb3bd
Vlatko Ivetic <vlatko.ivetic@fer.hr>
2020-07-16 15:58:56 +0200

    add test mock for redux store

--- app/views/__tests__/Licenses.spec.tsx
+++ app/views/__tests__/Licenses.spec.tsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import 'react-native';
-import { render } from '@testing-library/react-native';
+import { render } from '../../../tets-utils';
 import '@testing-library/jest-native/extend-expect';
 
 import { TracingStrategyProvider } from '../../TracingStrategyContext';
+import { TracingStrategy } from '../../tracingStrategy';
 import gpsStrategy from '../../gps';
 import btStrategy from '../../bt';
 
 import { LicensesScreen } from '../Licenses';
+import { FeatureFlagOption } from '../../store/types';
+
+const renderTracingStrategyProvider = (strategy: TracingStrategy) => {
+  return render(
+    <TracingStrategyProvider strategy={strategy}>
+      <LicensesScreen />
+    </TracingStrategyProvider>,
+    {
+      initialState: {
+        featureFlags: { flags: { [FeatureFlagOption.EXPOSURE_MODE]: false } },
+      },
+    },
+  );
+};
 
 describe('LicensesScreen', () => {
   it('renders correctly', () => {
-    const { asJSON } = render(
-      <TracingStrategyProvider strategy={gpsStrategy}>
-        <LicensesScreen />
-      </TracingStrategyProvider>,
-    );
-
+    const { asJSON } = renderTracingStrategyProvider(gpsStrategy);
     expect(asJSON()).toMatchSnapshot();
   });
 
   describe('when the tracing strategy is gps', () => {
     it('displays PathCheck GPS', () => {
-      const { getByTestId } = render(
-        <TracingStrategyProvider strategy={gpsStrategy}>
-          <LicensesScreen />
-        </TracingStrategyProvider>,
-      );
+      const { getByTestId } = renderTracingStrategyProvider(gpsStrategy);
 
       expect(getByTestId('licenses-legal-header')).toHaveTextContent(
         'PathCheck GPS',
@@ -36,11 +42,7 @@ describe('LicensesScreen', () => {
 
   describe('when the tracing strategy is bt', () => {
     it('displays PathCheck BT', () => {
-      const { getByTestId } = render(
-        <TracingStrategyProvider strategy={btStrategy}>
-          <LicensesScreen />
-        </TracingStrategyProvider>,
-      );
+      const { getByTestId } = renderTracingStrategyProvider(btStrategy);
 
       expect(getByTestId('licenses-legal-header')).toHaveTextContent(
         'PathCheck BT',
