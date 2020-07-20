328efdbf644f0a335bfb3daaf237c916649679d0
Jacob <JacobMaxJaffe@gmail.com>
2020-07-20 13:50:44 -0400

    tests fix

--- app/views/ExposureHistory/DateInfoHeader.spec.tsx
+++ app/views/ExposureHistory/DateInfoHeader.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, wait } from '@testing-library/react-native';
-import dayjs from 'dayjs';
+// import dayjs from 'dayjs';
 import '@testing-library/jest-native/extend-expect';
 
 import DateInfoHeader from './DateInfoHeader';
@@ -9,22 +9,23 @@ import ExposureHistoryContext, {
 } from '../../ExposureHistoryContext';
 
 describe('DateInfoHeader', () => {
-  it('displays the time since the last exposure detection', async () => {
-    const lastExposureDetectionDate = dayjs().subtract(8, 'hour');
+  //  TODO: reintroduce this test when we display this
+  // it('displays the time since the last exposure detection', async () => {
+  //   const lastExposureDetectionDate = dayjs().subtract(8, 'hour');
 
-    const { getByText } = render(
-      <ExposureHistoryContext.Provider
-        value={{ ...initialState, lastExposureDetectionDate }}>
-        <DateInfoHeader />
-      </ExposureHistoryContext.Provider>,
-    );
+  //   const { getByText } = render(
+  //     <ExposureHistoryContext.Provider
+  //       value={{ ...initialState, lastExposureDetectionDate }}>
+  //       <DateInfoHeader />
+  //     </ExposureHistoryContext.Provider>,
+  //   );
 
-    await wait(() => {
-      expect(
-        getByText(' • Updated 8 hours ago', { exact: false }),
-      ).toBeDefined();
-    });
-  });
+  //   await wait(() => {
+  //     expect(
+  //       getByText(' • Updated 8 hours ago', { exact: false }),
+  //     ).toBeDefined();
+  //   });
+  // });
 
   describe('when there is not an exposure detection date', () => {
     it('does not displays the date info', async () => {
