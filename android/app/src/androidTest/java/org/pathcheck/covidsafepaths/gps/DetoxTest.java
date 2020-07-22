1440722f8e90b08081018e1a656158b466de47ff
Juan Cruz Soler <juancruz.gsoler@gmail.com>
2020-07-22 16:38:32 -0300

    Remove BT from android codebase

--- android/app/src/androidTest/java/org/pathcheck/covidsafepaths/gps/DetoxTest.java
+++ android/app/src/androidTest/java/org/pathcheck/covidsafepaths/gps/DetoxTest.java
@@ -1,13 +1,15 @@
+package org.pathcheck.covidsafepaths.gps;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
 import com.wix.detox.Detox;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.pathcheck.covidsafepaths.MainActivity;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
