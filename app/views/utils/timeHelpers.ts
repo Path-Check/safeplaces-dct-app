dfe212101e00026ba0ef5e473e7172363cb343ce
yonatanmk <yonatanmk@gmail.com>
2020-07-22 16:19:26 -0400

    Display Correct Exposure Duration On Calendar Screen

--- app/views/utils/timeHelpers.ts
+++ app/views/utils/timeHelpers.ts
@@ -12,10 +12,11 @@ type DurationRoundedToFiveMinuteIncrement = number;
 export const durationToString = (
   duration: DurationRoundedToFiveMinuteIncrement,
 ): string => {
-  /// Native layer returns length of exposure in
-  /// 5 minute increments with a 30 minute maximum.
-  /// 1 is the smallest possible number of 5 minute increments,
-  /// so if we receive 1 from the native layer, we display "one minute"
-  const durationMinutes = Math.max(duration, 1);
+  // Duration is in milliseconds
+  // Native layer returns length of exposure in
+  // 5 minute increments with a 30 minute maximum.
+  // 1 is the smallest possible number of 5 minute increments,
+  // so if we receive 1 from the native layer, we display "one minute"
+  const durationMinutes = Math.max(duration / 1000 / 60, 1);
   return dayjs.duration({ minutes: durationMinutes }).humanize(false);
 };
